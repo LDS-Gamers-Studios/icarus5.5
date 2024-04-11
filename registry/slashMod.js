6415ea9ea44a45704aab441a442afe1dd7b6a006
BobbyTheCatfish <46359040+BobbyTheCatfish@users.noreply.github.com>
2024-04-11 14:54:24 -0600

    update to new format

--- registry/slashMod.js
+++ registry/slashMod.js
@@ -1,331 +1,196 @@
-const Discord = require('discord.js');
-const type = Discord.ApplicationCommandOptionType;
+// @ts-check
+const u = require("./regUtils");
 
-module.exports = {
-  "name": "mod",
-  "description": "Modding Actions Within LDSG",
-  "default_permission": false,
-  "type": Discord.ApplicationCommandType.ChatInput,
-  "options": [
-    {
-      "name": "ban",
-      "description": "Ban a user",
-      "type": type.Subcommand,
-      "options": [
-        {
-          "name": "user",
-          "description": "Who do you want to ban?",
-          "type": type.User,
-          "required": true
-        },
-        {
-          "name": "reason",
-          "description": "Why are they being banned?",
-          "type": type.String,
-          "required": true
-        },
-        {
-          "name": "clean",
-          "description": "How many days of messages should I remove? (Default: 1)",
-          "type": type.Integer,
-          "required": false
-        }
-      ]
-    },
-    {
-      "name": "filter",
-      "description": "Add or remove a word from the language filter",
-      "type": type.Subcommand,
-      "options": [
-        {
-          "name": "word",
-          "description": "Which word do you want to add to the language filter?",
-          "type": type.String,
-          "required": true
-        },
-        {
-          "name": "apply",
-          "description": "Should I add (`true`) or remove (`false`) the word? (Default: `true`)",
-          "type": type.Boolean,
-          "required": false
-        }
-      ]
-    },
-    {
-      "name": "fullinfo",
-      "description": "Check when a user joined the server and rank information",
-      "type": type.Subcommand,
-      "options": [
-        {
-          "name": "user",
-          "description": "Which user do you want info about?",
-          "type": type.User,
-          "required": false
-        },
-        {
-          "name": "history",
-          "description": "How many days history do you need? (Default `28`)",
-          "type": type.Integer,
-          "required": false
-        }
-      ]
-    },
-    {
-      "name": "kick",
-      "description": "Kick a user",
-      "type": type.Subcommand,
-      "options": [
-        {
-          "name": "user",
-          "description": "Who do you want to kick?",
-          "type": type.User,
-          "required": true
-        },
-        {
-          "name": "reason",
-          "description": "Why are they being kicked?",
-          "type": type.String,
-          "required": true
-        }
-      ]
-    },
-    {
-      "name": "mute",
-      "description": "Mute or unmute a user",
-      "type": type.Subcommand,
-      "options": [
-        {
-          "name": "user",
-          "description": "Who do you want to mute?",
-          "type": type.User,
-          "required": true
-        },
-        {
-          "name": "reason",
-          "description": "Why are they being muted?",
-          "type": type.String,
-          "required": false
-        },
-        {
-          "name": "apply",
-          "description": "Do I apply (`true`) or remove (`false`) the mute? (Default: `true`)",
-          "type": type.Boolean,
-          "required": false
-        }
-      ]
-    },
-    {
-      "name": "note",
-      "description": "Make a note about a user",
-      "type": type.Subcommand,
-      "options": [
-        {
-          "name": "user",
-          "description": "Who needs the note?",
-          "type": type.User,
-          "required": true
-        },
-        {
-          "name": "note",
-          "description": "What is the note?",
-          "type": type.String,
-          "required": true
-        }
-      ]
-    },
-    {
-      "name": "office",
-      "description": "Send a user to the office",
-      "type": type.Subcommand,
-      "options": [
-        {
-          "name": "user",
-          "description": "Who are you sending to the office?",
-          "type": type.User,
-          "required": true
-        },
-        {
-          "name": "reason",
-          "description": "Why are you sending them there?",
-          "type": type.String,
-          "required": true
-        },
-        {
-          "name": "apply",
-          "description": "Am I sending them (`true`) or letting them out (`false`)? (Default: `true`)",
-          "type": type.Boolean,
-          "required": false
-        }
-      ]
-    },
-    {
-      "name": "purge",
-      "description": "Purge messages in the channel",
-      "type": type.Subcommand,
-      "options": [
-        {
-          "name": "number",
-          "description": "How many messages should I delete?",
-          "type": type.Integer,
-          "required": true
-        },
-        {
-          "name": "reason",
-          "description": "Why are you purging?",
-          "type": type.String,
-          "required": true
-        }
-      ]
-    },
-    {
-      "name": "rename",
-      "description": "Change a user's nickname, if their name is inappropriate",
-      "type": type.Subcommand,
-      "options": [
-        {
-          "name": "user",
-          "description": "Who do you want to rename?",
-          "type": type.User,
-          "required": true
-        },
-        {
-          "name": "name",
-          "description": "What name should I apply?",
-          "type": type.String,
-          "required": false
-        }
-      ]
-    },
-    {
-      "name": "watchlist",
-      "description": "Shows the trusted but watched members",
-      "type": type.Subcommand,
-      "options": []
-    },
-    {
-      "name": "slowmode",
-      "description": "Set a temporary slow mode on the channel",
-      "type": type.Subcommand,
-      "options": [
-        {
-          "name": "channel",
-          "description": "Which channel needs a slowmode?",
-          "type": type.Channel,
-          "required": false
-        },
-        {
-          "name": "duration",
-          "description": "How many minutes will it last? (Default: `10`)",
-          "type": type.Integer,
-          "required": false
-        },
-        {
-          "name": "timer",
-          "description": "How many seconds between messages? (Default: `15`)",
-          "type": type.Integer,
-          "required": false
-        },
-        {
-          "name": "indefinitely",
-          "description": "Enable slowmode indefinitely",
-          "type": type.Boolean,
-          "required": false
-        }
-      ]
-    },
-    {
-      "name": "summary",
-      "description": "Get a brief summary of a user's history",
-      "type": type.Subcommand,
-      "options": [
-        {
-          "name": "user",
-          "description": "Whose summary do you want to see?",
-          "type": type.User,
-          "required": true
-        },
-        {
-          "name": "history",
-          "description": "How many days history do you need? (Default `28`)",
-          "type": type.Integer,
-          "required": false
-        }
-      ]
-    },
-    {
-      "name": "trust",
-      "description": "Trust or untrust a user",
-      "type": type.Subcommand,
-      "options": [
-        {
-          "name": "user",
-          "description": "Which user are you applying this to?",
-          "type": type.User,
-          "required": true
-        },
-        {
-          "name": "type",
-          "description": "What type of Trusting is needed?",
-          "type": type.String,
-          "choices": [
-            { "name": "Initial", "value": "initial" },
-            { "name": "Plus", "value": "plus" },
-            { "name": "Watch", "value": "watch" }
-          ],
-          "required": true
-        },
-        {
-          "name": "apply",
-          "description": "Should I apply (`true`) or remove (`false`) the role? (Default: `true`)",
-          "type": type.Boolean,
-          "required": false
-        }
-      ]
-    },
-    {
-      "name": "warn",
-      "description": "Give a user a warning",
-      "type": type.Subcommand,
-      "options": [
-        {
-          "name": "user",
-          "description": "Which user do you want to warn?",
-          "type": type.User,
-          "required": true
-        },
-        {
-          "name": "reason",
-          "description": "Why do you need to warn them?",
-          "type": type.String,
-          "required": true
-        },
-        {
-          "name": "value",
-          "description": "What value is the warning? (Default: `1`)",
-          "type": type.Integer,
-          "required": false
-        }
-      ]
-    },
-    {
-      "name": "watch",
-      "description": "Watchlist for LDSG",
-      "default_permission": false,
-      "type": type.Subcommand,
-      "options": [
-        {
-          "name": "user",
-          "description": "Who do you want to put on the watchlist?",
-          "type": type.User,
-          "required": true,
-        },
-        {
-          "name": "apply",
-          "description": "Apply role (default: true)",
-          "type": type.Boolean,
-          "required": false,
-        }
-      ]
-    }
-  ]
-};
+/** Who do you want to X? */
+const user = (action, req = true) => new u.user()
+  .setName('user')
+  .setDescription(`Who do you want to ${action}?`)
+  .setRequired(req);
+
+/** Why are they being X? */
+const reason = (action, req = true) => new u.string()
+  .setName("reason")
+  .setDescription(`Why are they being ${action}?`)
+  .setRequired(req);
+
+/** Should I add (true) or remove (false) the X? (Default: Y) */
+const apply = (obj, def = "add") => new u.bool()
+  .setName("apply")
+  .setDescription(`"Should I add (\`true\`) or remove (\`false\`) the ${obj}? (Default: \`(${def}\`)`)
+  .setRequired(false);
+
+const ban = new u.sub()
+  .setName("ban")
+  .setDescription("Ban a user")
+  .addUserOption(user("ban"))
+  .addStringOption(reason("banned"))
+  .addIntegerOption(
+    new u.int()
+      .setName("clean")
+      .setDescription("How many days of messages should I remove? (Default: 1)")
+      .setRequired(false)
+      .setMinValue(0)
+      .setMaxValue(7)
+  );
+
+const filter = new u.sub()
+  .setName("filter")
+  .setDescription("Add or remove a word from the language filter")
+  .addStringOption(
+    new u.string()
+      .setName("word")
+      .setDescription("Which word do you want to modify?")
+      .setRequired(true)
+  )
+  .addBooleanOption(apply('word', 'add'));
+
+const summary = new u.sub()
+  .setName("summary")
+  .setDescription("Check user details and infractions")
+  .addUserOption(user("get info on"))
+  .addIntegerOption(
+    new u.int()
+      .setName("history")
+      .setDescription("How many days history do you need? (Default: 28)")
+      .setMinValue(1)
+      .setRequired(false)
+  );
+
+const kick = new u.sub()
+  .setName("kick")
+  .setDescription("Kick a user")
+  .addUserOption(user("kick"))
+  .addStringOption(reason("kicked"));
+
+const mute = new u.sub()
+  .setName("mute")
+  .setDescription("Mute or unmute a user")
+  .addUserOption(user("mute"))
+  .addStringOption(reason("muted", false))
+  .addBooleanOption(apply("mute", "apply"));
+
+const note = new u.sub()
+  .setName("note")
+  .setDescription("Make a note about a user")
+  .addUserOption(user("write a note about"))
+  .addStringOption(
+    new u.string()
+      .setName("note")
+      .setDescription("What is the note?")
+      .setRequired(true)
+  );
+
+const office = new u.sub()
+  .setName("office")
+  .setDescription("Send a user to the office")
+  .addUserOption(user("send to the office"))
+  .addStringOption(reason("sent there"))
+  .addBooleanOption(apply("user from the office", "add"));
+
+const purge = new u.sub()
+  .setName("purge")
+  .setDescription("Purge messages in the channel")
+  .addIntegerOption(
+    new u.int()
+      .setName("number")
+      .setDescription("How many messages should I delete?")
+      .setRequired(true)
+      .setMinValue(1)
+      .setMaxValue(100)
+  )
+  .addStringOption(reason("purged"));
+
+const rename = new u.sub()
+  .setName("rename")
+  .setDescription("Change a user's nickname")
+  .addUserOption(user('rename'))
+  .addStringOption(
+    new u.string()
+      .setName("name")
+      .setDescription("What name should I apply? (Defaults to a random silly name)")
+      .setRequired(false)
+  );
+
+const watchlist = new u.sub()
+  .setName("watchlist")
+  .setDescription("Shows the trusted but watched members");
+
+const slowmode = new u.sub()
+  .setName("slowmode")
+  .setDescription("Set a temporary slow mode in the current channel")
+  .addIntegerOption(
+    new u.int()
+      .setName("duration")
+      .setDescription("How many minutes will it last? (Default: `10`)")
+      .setMinValue(0)
+      .setRequired(false)
+  )
+  .addIntegerOption(
+    new u.int()
+      .setName("delay")
+      .setDescription("How many seconds between messages? (Default: `15`)")
+      .setMinValue(0)
+      .setRequired(false)
+  )
+  .addBooleanOption(
+    new u.bool()
+      .setName("indefinite")
+      .setDescription("Enables slowmode forever")
+      .setRequired(false)
+  );
+
+const trust = new u.sub()
+  .setName("trust")
+  .setDescription("Trust, Trust+, or untrust a user")
+  .addUserOption(user("apply this to"))
+  .addStringOption(
+    new u.string()
+      .setName("type")
+      .setDescription("What type of trusting is needed?")
+      .setChoices(
+        { name: "Initial", value: "initial" },
+        { name: "Plus", value: "plus" }
+      )
+      .setRequired(true)
+  )
+  .addBooleanOption(apply("role", "add"));
+
+const warn = new u.sub()
+  .setName("warn")
+  .setDescription("Give a user a warning")
+  .addUserOption(user("warn"))
+  .addStringOption(reason("warned"))
+  .addIntegerOption(
+    new u.int()
+      .setName("value")
+      .setDescription("What value is their warning? (Default: `1`)")
+      .setRequired(false)
+      .setMinValue(1)
+  );
+
+const watch = new u.sub()
+  .setName("watch")
+  .setDescription("Add or remove a user from the watchlist")
+  .addUserOption(user("put on the watchlist"))
+  .addBooleanOption(apply("user from the watchlist", "add"));
+
+module.exports = new u.cmd()
+  .setName("mod")
+  .setDescription("Modding actions within LDSG")
+  .addSubcommand(ban)
+  .addSubcommand(filter)
+  .addSubcommand(kick)
+  .addSubcommand(mute)
+  .addSubcommand(note)
+  .addSubcommand(office)
+  .addSubcommand(purge)
+  .addSubcommand(rename)
+  .addSubcommand(watchlist)
+  .addSubcommand(slowmode)
+  .addSubcommand(summary)
+  .addSubcommand(trust)
+  .addSubcommand(warn)
+  .addSubcommand(watch)
+  .setDMPermission(false)
+  .setDefaultMemberPermissions(0)
+  .toJSON();
