696bfc8427ed4661030d2764cda8ee5b0c01b2c3
BobbyTheCatfish <46359040+BobbyTheCatfish@users.noreply.github.com>
2024-04-29 21:37:40 -0600

    remove some comments

--- modules/banners.js
+++ modules/banners.js
@@ -5,21 +5,21 @@ const u = require("../utils/utils"),
   Augur = require("augurbot-ts"),
   banners = require('../data/banners.json');
 
-function setBanner(holiday) { // !! added an optional parameter to set a banner
+function setBanner(holiday) {
   const date = new Date();
   const month = date.getMonth();
   const day = date.getDate();
 
   // should look for the banners in banners.json
   const banner = banners.find(b => holiday ? b.file == holiday.toLowerCase() : b.month === month && b.day === day);
-  if (!banner) return; // !! end function here if there's not a banner
+  if (!banner) return; // end function here if there's not a banner
 
   const bannerPath = `media/banners/${banner.file}.png`;
 
   const ldsg = Module.client.guilds.cache.get(u.sf.ldsg);
   if (!ldsg) return u.errorHandler(new Error("LDSG is unavailable. (Banner Set)"));
 
-  // !! they only need a notification if it fails
+  // notify management if setting fails
   ldsg.setBanner(bannerPath).catch(() => {
     Module.client.getTextChannel(u.sf.channels.management)?.send({
       content: `Failed to set banner, please do this manually.`,
@@ -40,5 +40,5 @@ const Module = new Augur.Module()
       setBanner(suffix);
     }
   });
-// :sniffa:
+
 module.exports = Module;
