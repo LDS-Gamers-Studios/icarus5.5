60cd606080e20d21e9dc8fd7d078a4e0e2023c61
OhGo <56556525+LDSGOhGo@users.noreply.github.com>
2024-05-03 21:57:47 -0600

    add suggested changes
    add suggested changes from review

--- modules/roles.js
+++ modules/roles.js
@@ -1,13 +1,12 @@
 // @ts-check
 const Augur = require("augurbot-ts");
 const u = require("../utils/utils");
-const p = require("../utils/perms");
 const config = require("../config/config.json");
 const { GoogleSpreadsheet } = require("google-spreadsheet");
 const Discord = require("discord.js");
 
-const roles = new u.Collection;
-let eRoles = new u.Collection;
+const roles = new u.Collection();
+let eRoles = new u.Collection();
 
 /**
  * @param {Augur.GuildInteraction<"CommandSlash">} int
@@ -15,36 +14,42 @@ let eRoles = new u.Collection;
 */
 async function roleFunc(int, give = true) {
   try {
-    const input = int.options.getString("role");
-    const ldsg = Module.client.guilds.cache.get(u.sf.ldsg);
-    if (p.isAdmin(int.member)) {
-      const member = ldsg?.members.cache.find(usr => usr.id === int.user.id);
-      const role = ldsg?.roles.cache.find(r => r.name.toLowerCase() == input?.toLowerCase());
+    await int.deferReply({ ephemeral: true });
+    const input = int.options.getString("role", true);
+    if (u.perms.isAdmin(int.member)) {
+      const member = int.guild.members.cache.find(usr => usr.id === int.user.id);
+      const role = int.guild.roles.cache.find(r => r.name.toLowerCase() == input.toLowerCase());
       if (role) {
         if (give) {
           try {
-            if (member?.roles.cache.find(r => r.id == role.id)) return int.reply({ content: `You already have the ${role.name.toLowerCase()} role`, ephemeral: true });
+            if (member?.roles.cache.has(role.id)) return int.editReply(`You already have the ${role} role`);
             await member?.roles.add(role);
-            return int.reply({ content: `Successfully added the ${role.name} role`, ephemeral: true });
-          } catch (e) { return int.reply({ content: `Failed to add the ${role.name} role`, ephemeral: true }); }
+            return int.editReply(`Successfully added the ${role} role`);
+          } catch (e) { return int.editReply(`Failed to add the ${role} role`); }
         }
         try {
-          if (!member?.roles.cache.find(r => r.id == role.id)) return int.reply({ content: `You do not have the ${role.name.toLowerCase()} role`, ephemeral: true });
+          if (!member?.roles.cache.has(role.id)) return int.editReply(`You do not have the ${role} role`);
           await member?.roles.remove(role);
-          return int.reply({ content: `Successfully removed the ${role.name} role`, ephemeral: true });
-        } catch (e) { return int.reply({ content: `Failed to remove the ${role.name} role`, ephemeral: true }); }
-      } else { return int.reply({ content: `I couldn't find the ${input} role`, ephemeral: true }); }
-    } else if (roles.has(input?.toLowerCase())) {
-      const role = ldsg.roles.cache.get(roles.get(input?.toLowerCase()));
-      const member = await ldsg.members.fetch(int.user.id);
-      if (give) {
-        if (member) await member.roles.add(role);
-        return int.reply({ content: `Successfully added the ${role.name} role`, ephemeral: true });
+          return int.editReply(`Successfully removed the ${role} role`);
+        } catch (e) { return int.editReply(`Failed to remove the ${role} role`); }
+      } else { return int.editReply(`I couldn't find the ${input} role`); }
+    } else if (roles.has(input.toLowerCase())) {
+      const role = int.guild.roles.cache.get(roles.get(input.toLowerCase()));
+      const member = await int.guild.members.fetch(int.user.id);
+      if (role) {
+        if (give) {
+          try {
+            if (member) await member.roles.add(role);
+            return int.editReply(`Successfully added the ${role} role`);
+          } catch (e) { return int.editReply(`Failed to add the ${role} role`); }
+        }
+        try {
+          if (member) await member.roles.remove(role);
+          return int.editReply(`Successfully removed the ${role} role`);
+        } catch (e) { return int.reply(`Failed to remove the ${role} role`); }
       }
-      if (member) await member.roles.remove(role);
-      return int.reply({ content: `Successfully removed the ${role.name} role`, ephemeral: true });
     } else {
-      return int.reply({ content: `You didn't give me a valid role`, ephemeral: true });
+      return int.editReply(`You didn't give me a valid role`);
     }
   } catch (error) { u.errorHandler(error, int); }
 }
@@ -52,14 +57,13 @@ async function roleFunc(int, give = true) {
 /** @param {Augur.GuildInteraction<"CommandSlash">} int */
 async function whoHas(int) {
   try {
-    const input = int.options.getString("role");
-    const ldsg = Module.client.guilds.cache.get(u.sf.ldsg);
-    const role = ldsg.roles.cache.find(r => r.name.toLowerCase() == input?.toLowerCase());
+    const input = int.options.getString("role", true);
+    const role = int.guild.roles.cache.find(r => r.name.toLowerCase() == input.toLowerCase());
     if (role) {
-      if (role && role.members.size > 0) int.reply({ content: `Members with the ${role.name} role:\n\`\`\`\n${role.members.map(m => m.displayName).sort().join("\n")}\n\`\`\``, ephemeral: !(int.channel?.id == int.client.getTextChannel(u.sf.channels.general)?.id) });
-      else int.reply("I couldn't find any members with that role. :shrug:");
+      if (role && role.members.size > 0) int.reply({ content: `Members with the ${role} role:\n\`\`\`\n${role.members.map(m => m.displayName).sort().join("\n")}\n\`\`\``, ephemeral: !(int.channel?.id == int.client.getTextChannel(u.sf.channels.general)?.id) });
+      else int.reply({ content: "I couldn't find any members with that role. :shrug:", ephemeral: true });
     } else {
-      int.reply("You need to give me a valid role to find!");
+      int.reply({ content: "You need to give me a valid role to find!", ephemeral: true });
       u.clean(int);
     }
   } catch (error) { u.errorHandler(error, int); }
@@ -71,66 +75,29 @@ async function whoHas(int) {
 */
 async function staffFunc(int, give = true) {
   try {
-    const input = int.options.getString("role");
-    if (!p.calc(int.member, ["team", "mod", "mgr"])) return int.reply({ content: "*Nice try!* This command is for Team+ only", ephemeral: true });
-    if (!p.calc(int.member, ["mod", "mgr"]) && input?.toLowerCase() == "adulting") return int.reply({ content: "This command is for Mod+ only", ephemeral: true });
+    const pres = give ? "give" : "take";
+    const past = give ? "gave" : "removed";
+    await int.deferReply({ ephemeral: true });
+    const input = int.options.getString("role", true);
+    if (!u.perms.calc(int.member, ["team", "mod", "mgr"])) return int.editReply("*Nice try!* This command is for Team+ only");
+    if (!u.perms.calc(int.member, ["mod", "mgr"]) && input.toLowerCase() == "adulting") return int.editReply("This command is for Mod+ only");
     const recipient = int.options.getMember("user");
-    const ldsg = Module.client.guilds.cache.get(u.sf.ldsg);
-    const role = ldsg.roles.cache.find(r => r.name.toLowerCase() == input?.toLowerCase());
+    const role = int.guild.roles.cache.find(r => r.name.toLowerCase() == input.toLowerCase());
     if (role) {
-      switch (role.name.toLowerCase()) {
-      case "adulting":
-        if (give) {
-          try {
-            await recipient?.roles.add(u.sf.roles.adulting);
-            int.reply({ content: `Successfully gave the ${role.name} role`, ephemeral: true });
-            const embed = u.embed({ author: recipient, color: 0x5865f2 });
-            embed.setTitle("User Added to Adulting")
-            .setDescription(`${int.member} added the <@&${u.sf.roles.adulting}> role to ${recipient}.`);
-            return int.client.getTextChannel(u.sf.channels.modlogs)?.send({ embeds: [embed] });
-          } catch (e) { return int.reply({ content: `Failed to give the ${role.name} role`, ephemeral: true }); }
-        }
-        try {
-          await recipient?.roles.remove(u.sf.roles.adulting);
-          int.reply({ content: `Successfully removed the ${role.name} role`, ephemeral: true });
-          const embed = u.embed({ author: recipient, color: 0x5865f2 });
-          embed.setTitle("User Removed from Adulting")
-            .setDescription(`${int.member} removed the <@&${u.sf.roles.adulting}> role from ${recipient}.`);
-          return int.client.getTextChannel(u.sf.channels.modlogs)?.send({ embeds: [embed] });
-        } catch (e) { return int.reply({ content: `Failed to take the ${role.name} role`, ephemeral: true }); }
-      case "ldsg lady":
-        if (give) {
-          try {
-            await recipient?.roles.add(u.sf.roles.lady);
-            int.reply({ content: `Successfully gave the ${role.name} role`, ephemeral: true });
-            const embed = u.embed({ author: recipient, color: 0x5865f2 });
-            embed.setTitle("User Added to LDSG Ladies")
-              .setDescription(`${int.member} added the <@&${u.sf.roles.lady}> role to ${recipient}.`);
-            return int.client.getTextChannel(u.sf.channels.modlogs)?.send({ embeds: [embed] });
-          } catch (e) { return int.reply({ content: `Failed to give the ${role.name} role`, ephemeral: true }); }
-        }
-        try {
-          await recipient?.roles.remove(u.sf.roles.lady);
-          int.reply({ content: `Successfully removed the ${role.name} role`, ephemeral: true });
-          const embed = u.embed({ author: recipient, color: 0x5865f2 });
-          embed.setTitle("User Removed from LDSG Ladies")
-            .setDescription(`${int.member} removed the <@&${u.sf.roles.lady}> role from ${recipient}.`);
-          return int.client.getTextChannel(u.sf.channels.modlogs)?.send({ embeds: [embed] });
-        } catch (e) { return int.reply({ content: `Failed to take the ${role.name} role`, ephemeral: true }); }
-      case "bookworm":
-        if (give) {
-          try {
-            await recipient?.roles.add(u.sf.roles.bookworm);
-            return int.reply({ content: `Successfully gave the ${role.name} role`, ephemeral: true });
-          } catch (e) { return int.reply({ content: `Failed to give the ${role.name} role`, ephemeral: true }); }
-        }
-        try {
-          await recipient?.roles.remove(u.sf.roles.bookworm);
-          return int.reply({ content: `Successfully removed the ${role.name} role`, ephemeral: true });
-        } catch (e) { return int.reply({ content: `Failed to remove the ${role.name} role`, ephemeral: true }); }
+      if (!(role.id == u.sf.roles.adulting || role.id == u.sf.roles.lady || role.id == u.sf.roles.bookworm)) {
+        return int.editReply(`This command is not for the ${role} role`);
       }
-    } else {
-      int.reply({ content:"You need to give me a valid role!", ephemeral: true });
+      try {
+        if (recipient?.roles.cache.has(role.id) && give) return int.editReply(`User already has the ${role} role`);
+        if (!recipient?.roles.cache.has(role.id) && !give) return int.editReply(`User already does not have the ${role} role`);
+        give ? await recipient?.roles.add(role.id) : await recipient?.roles.remove(role.id);
+        int.editReply(`Successfully ${past} the ${role} role`);
+        if (role.id == u.sf.roles.bookworm) return;
+        const embed = u.embed({ author: recipient, color: 0x5865f2 });
+        embed.setTitle(`User ${give ? "added to" : "removed from"} ${role.name}`)
+          .setDescription(`${int.member} ${past} the ${role} role ${give ? "to" : "from"} ${recipient}.`);
+        return int.client.getTextChannel(u.sf.channels.modlogs)?.send({ embeds: [embed] });
+      } catch (e) { return int.editReply(`Failed to ${pres} the ${role} role`); }
     }
   } catch (error) { u.errorHandler(error, int); }
 }
@@ -144,9 +111,9 @@ async function inventory(int) {
   try {
     const member = int.member;
     const inv = getInventory(member).map(color => int.guild.roles.cache.get(color.id)).sort((a, b) => b.comparePositionTo(a));
-    const embed = u.embed().setAuthor({ name: member.displayName, iconURL: member.displayAvatarURL({ size: 32 }) })
+    const embed = u.embed({ author: member })
       .setTitle("Equippable Color Inventory")
-      .setDescription(`Equip a color role with \`/role equip Role Name\`\ne.g. \`/role equip novice\`\n\n${inv.join("\n")}`);
+      .setDescription(`Equip a color role with </role equip:${u.sf.commands.slashRole}> \ne.g.  \`/role equip color:novice colors\`\n\n${inv.join("\n")}`);
     if (inv.length == 0) int.reply({ content: "You don't have any colors in your inventory!", ephemeral: true });
     else int.reply({ embeds: [embed], ephemeral: !(int.channel?.id == int.client.getTextChannel(u.sf.channels.botspam)?.id) });
   } catch (e) { u.errorHandler(e, int); }
@@ -155,24 +122,24 @@ async function inventory(int) {
 /** @param {Augur.GuildInteraction<"CommandSlash">} int */
 async function equip(int) {
   try {
-    const allColors = eRoles.filter(r => r.id).map(r => r.id);
+    int.deferReply({ ephemeral: true });
+    const allColors = eRoles.map(r => r.id);
     const available = getInventory(int.member);
     const input = int.options.getString("color");
     if (!input) {
       await int.member.roles.remove(allColors);
-      return int.reply({ content: "Color removed!", ephemeral: true });
+      return int.editReply("Color removed!");
     }
-    const ldsg = Module.client.guilds.cache.get(u.sf.ldsg);
-    const real = ldsg?.roles.cache.find(r => r.name.toLowerCase() == input.toLowerCase());
-    const role = ldsg?.roles.cache.find(r => r.id == eRoles.find(a => a.baseRole == real?.id || a.id == real?.id)?.id);
+    const real = int.guild.roles.cache.find(r => r.name.toLowerCase() == input.toLowerCase());
+    const role = int.guild.roles.cache.find(r => r.id == eRoles.find(a => a.baseRole == real?.id || a.id == real?.id)?.id);
     if (!role) {
-      return int.reply({ content: "Sorry, that's not a color role on this server. Check `/role inventory` to see what you can equip.", ephemeral: true });
+      return int.editReply("Sorry, that's not a color role on this server. Check `/role inventory` to see what you can equip.");
     } else if (!available.has(role.id)) {
-      return int.reply({ content: "Sorry, you don't have that color in your inventory. Check `/role inventory` to see what you can equip.", ephemeral: true });
+      return int.editReply("Sorry, you don't have that color in your inventory. Check `/role inventory` to see what you can equip.");
     } else {
       await int.member.roles.remove(allColors);
       await int.member.roles.add(role.id);
-      int.reply({ content: "Color applied!", ephemeral: true });
+      int.editReply("Color applied!");
     }
   } catch (e) { u.errorHandler(e, int); }
 }
@@ -187,12 +154,12 @@ const Module = new Augur.Module()
       if (interaction.isAutocomplete()) return;
       switch (interaction.options.getSubcommand(true)) {
       case "add": return await roleFunc(interaction);
-      case "give": return staffFunc(interaction);
-      case "equip": return equip(interaction);
-      case "inventory": return inventory(interaction);
       case "remove": return await roleFunc(interaction, false);
-      case "whohas": return whoHas(interaction);
+      case "give": return staffFunc(interaction);
       case "take": return staffFunc(interaction, false);
+      case "inventory": return inventory(interaction);
+      case "equip": return equip(interaction);
+      case "whohas": return whoHas(interaction);
       }
     },
     autocomplete: (interaction) => {
@@ -202,7 +169,7 @@ const Module = new Augur.Module()
           const all = Module.client.guilds.cache.get(u.sf.ldsg).roles.cache.filter((n) => n?.name.toLowerCase().startsWith(option.value?.toLowerCase())).map((n) => n.name).slice(0, 24);
           return interaction.respond(u.unique(all).map(v => ({ name: v, value: v })));
         } else if (interaction.options.getSubcommand(true) == "give" || interaction.options.getSubcommand(true) == "take") {
-          if (!p.calc(interaction.member, ["team", "mod", "mgr"])) return;
+          if (!u.perms.calc(interaction.member, ["team", "mod", "mgr"])) return;
           const values = ["Adulting", "Bookworm", "LDSG Lady"].filter(n => n?.toLowerCase().startsWith(option.value?.toLowerCase())).slice(0, 24);
           return interaction.respond(values.map(v => ({ name: v, value: v })));
         }
@@ -228,7 +195,7 @@ const Module = new Augur.Module()
       }
       // @ts-ignore
       const channels = await doc.sheetsByTitle["Roles"].getRows();
-      eRoles = new u.Collection(channels.map(r => [r['Color Role ID'], { id: r['Color Role ID'], baseRole: r['Base Role ID'], local: r['Local ID'], inherited: r['Lower Roles']?.split(" ") }])).filter(r => r.id != "");
+      eRoles = new u.Collection(channels.map(r => [r['Color Role ID'], { id: r['Color Role ID'], baseRole: r['Base Role ID'], local: r['Local ID'], inherited: r['Lower Roles']?.split(" ") }])).filter(r => r.id != "").filter(r => r.id);
     } catch (e) { u.errorHandler(e, "Load Color & Equip Roles"); }
   });
 
