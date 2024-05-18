406ad1ea50d75d3d2006da9c853a23a95d862367
Orrin Jelo <orrin.jelo@gmail.com>
2024-05-17 18:20:39 -0600

    Partial implementation of Elite commands.  Stashing for main branch merge.

--- modules/games.js
+++ modules/games.js
@@ -1,8 +1,8 @@
 // @ts-check
 const Augur = require("augurbot-ts"),
   { GoogleSpreadsheet } = require('google-spreadsheet'),
-  // eliteAPI = require('../utils/EliteApi'),
-  // axios = require('axios'),
+  eliteAPI = require('../utils/EliteApi'),
+  axios = require('axios'),
   discord = require('discord.js'),
   moment = require('moment'),
   perms = require('../utils/perms'),
@@ -119,7 +119,6 @@ async function slashGameDestiny(inter) {
     // }
   };
   const setValiant = async () => {
-    // console.log()
     if (!perms.isDestinyAdmin(inter.member) && !perms.isDestinyManager(inter.member) && !perms.isDestinyValiantAdmin(inter.member)) {
       return inter.reply({ content: `Only <@&${u.sf.roles.destinyvaliantadmin}> and above can use this command.`, ephemeral: true });
     }
@@ -141,6 +140,212 @@ async function slashGameDestiny(inter) {
   }
 }
 
+/**
+ * Send the image as an embed
+ * @param {discord.ChatInputCommandInteraction} int
+ * @param {Buffer | String} img
+ * @param {String} name
+ * @param {String} format
+ * @returns {Promise<any>}
+ */
+async function sendImg(int, img, name, format = "png") {
+  const image = u.attachment().setFile(img).setName(`image.${format}`);
+  const embed = u.embed().setTitle(name).setImage(`attachment://image.${format}`);
+  return int.reply({ embeds: [embed], files: [image] });
+}
+
+/** @param {Augur.GuildInteraction<"CommandSlash">} inter */
+async function slashGameMinecraftSkin(inter) {
+  // await inter.deferReply();
+  const user = inter.options.getMember('user') ?? inter.user;
+  const name = inter.options.getString('username') || (await u.db.ign.find(user?.id, 'minecraft'))?.ign;
+  if (!name) return inter.reply({ content: `${user} has not saved an IGN for Minecraft`, ephemeral: true });
+  try {
+    const headers = {
+      'User-Agent': 'Mozilla/5.0 (compatible; Discordbot/2.0; +https://discord.com)',
+      'Accept-Encoding': 'gzip, deflate',
+      'Accept': '*/*',
+      'Connection': 'keep-alive',
+      'content-type': 'image/png',
+    };
+    const uuid = (await axios.get(`https://api.mojang.com/users/profiles/minecraft/${name}`, { headers }))?.data;
+    if (!uuid?.id) return inter.reply({ content: `I couldn't find the player \`${name}\`.`, ephemeral: true });
+    const skinUrl = `https://visage.surgeplay.com/full/512/${uuid.id}.png`;
+    const result = await axios.get(`${skinUrl}`, { headers, responseType: 'arraybuffer' });
+    if (result.status == 200) {
+      return sendImg(inter, Buffer.from(result.data, 'binary'), "minecraft_skin.png", "png");
+    } else {
+      return inter.reply({ content: "An error occurred obtaining the skin for that player." });
+    }
+  } catch (error) {
+    return inter.reply({ content: `I couldn't find the player \`${name}\`.`, ephemeral: true });
+  }
+}
+
+/**
+ * @param {Augur.GuildInteraction<"CommandSlash">} inter
+ * @param {string} game
+ */
+function currentPlayers(inter, game) {
+  const players = inter.guild.members.cache.map(m => {
+    if (m.user.bot) return null;
+    const presence = m.presence?.activities?.find(a => a.type == discord.ActivityType.Playing && a.name.toLowerCase().startsWith(game.toLowerCase()));
+    return presence ? `• ${m}` : null;
+  }).filter(p => p != null).sort((a, b) => a.localeCompare(b));
+  return u.embed().setTitle(`${inter.guild.name} members currently playing ${game}`).setDescription(players.length > 0 ? players.join('\n') : `I couldn't find any members playing ${game}`);
+}
+
+/** @param {Augur.GuildInteraction<"CommandSlash">} inter */
+async function slashGameGetPlaying(inter) {
+  const game = inter.options.getString("game") ?? gameDefaults.get(inter.channel?.id)?.game;
+  if (game) return inter.reply({ embeds: [currentPlayers(inter, game)], ephemeral: true });
+  // List *all* games played
+  const games = new u.Collection();
+  for (const [, member] of inter.guild.members.cache) {
+    if (member.user.bot) continue;
+    const playing = member.presence?.activities?.find(a => a.type == discord.ActivityType.Playing);
+    if (playing && !games.has(playing.name)) games.set(playing.name, { game: playing.name, players: 0 });
+    if (playing) games.get(playing.name).players++;
+  }
+
+  const gameList = games.sort((a, b) => {
+    if (b.players == a.players) return a.game.localeCompare(b.game);
+    else return b.players - a.players;
+  }).toJSON();
+  const s = gameList.length > 0 ? 's' : '';
+  const embed = u.embed().setTimestamp()
+    .setTitle(`Currently played game${s} in ${inter.guild.name}`)
+    .setDescription(`The top ${Math.min(gameList.length, 25)} game${s} currently being played in ${inter.guild.name}:`);
+  if (gameList.length > 0) gameList.map((g, i) => i < 25 ? embed.addFields({ name: g.game, value: `${g.players}` }) : null);
+  else embed.setDescription("Well, this is awkward ... Nobody is playing anything.");
+  inter.reply({ embeds: [embed], ephemeral: true });
+}
+
+/** @param {Augur.GuildInteraction<"CommandSlash">} inter */
+async function slashGameElite(inter) {
+  const starSystem = inter.options.getString('system-name') ? await eliteAPI.getSystemInfo(inter.options.getString('system-name')) : null;
+  const embed = u.embed().setThumbnail("https://i.imgur.com/Ud8MOzY.png").setAuthor({ name: "EDSM", iconURL: "https://i.imgur.com/4NsBfKl.png" });
+  const info = inter.options.getString('info');
+  let reply;
+  if (info && !['status', 'time'].includes(info) && !starSystem) {
+    if (!inter.options.getString('system-name')) reply = "You need to give me a system name to look up.";
+    else reply = "I couldn't find a system with that name.";
+    return inter.reply({ content: reply, ephemeral: true });
+  }
+
+  switch (info) {
+  case "status": return inter.reply( await eliteGetStatus() ); break;
+  case "time": return inter.reply( eliteGetTime() ); break;
+  case "bodies": reply = await eliteGetBodies(starSystem, embed); break;
+  case "factions": reply = await eliteGetFactions(starSystem, embed); break;
+  case "stations": reply = await eliteGetStations(starSystem, embed); break;
+  case "system": return inter.reply( await eliteGetSystem(starSystem, embed) ); break;
+  }
+  return inter.reply(reply);
+}
+
+async function eliteGetStatus() {
+  const status = await eliteAPI.getEliteStatus();
+  return { content: `The Elite: Dangerous servers are ${status.type == 'success' ? "online" : "offline"}`, ephemeral: true };
+}
+
+function eliteGetTime() {
+  const d = new Date();
+  const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  return { content: `The current date/time in Elite is ${monthNames[d.getUTCMonth()]} ${d.getUTCDate()}, ${(d.getUTCFullYear() + 1286)}, ${d.getUTCHours()}:${d.getUTCMinutes()}. (UTC + 1286 years)`, ephemeral: true };
+}
+
+/**
+ * @param {string} system
+ * @param {discord.EmbedBuilder} embed
+ */
+async function eliteGetSystem(system, embed) {
+  embed.setTitle(system.name)
+    .setURL(`https://www.edsm.net/en/system/id/${system.id}/name`)
+    .addFields({
+      name: 'Permit Required?', 
+      value: system.requirePermit ? "Yes" : "No", true
+    });
+  if (system.primaryStar)embed.addField("Star Scoopable", system.primaryStar.isScoopable ? "Yes" : "No", true);
+
+  if (system.information) {
+    embed.addField("Controlling Faction", system.information.faction, true)
+      .addField("Government Type", system.information.allegiance + " - " + system.information.government, true);
+  } else {
+    embed.addField("Uninhabited System", "No faction information available.", true);
+  }
+  return { embeds: [embed] };
+}
+
+/**
+ * @param {string} system
+ * @param {discord.MessageEmbed} embed
+ */
+async function eliteGetStations(system, embed) {
+  if (system.stations.length <= 0) return { content: "I couldn't find any stations in that system.", ephemeral: true };
+  embed.setTitle(system.name).setURL(system.stationsURL);
+
+  const stationList = new Map();
+  for (let i = 0; i < Math.min(system.stations.length, 25); i++) {
+    const station = system.stations[i];
+    // Filtering out fleet carriers. There can be over 100 of them (spam) and their names are user-determined (not always clean).
+    if (!["Fleet Carrier", "Unknown"].includes(station.type)) {
+      if (!stationList.has(station.type)) stationList.set(station.type, []);
+      stationList.get(station.type).push(station);
+    }
+  }
+
+  for (const [stationType, stations] of stationList) {
+    embed.addField(stationType, "-----------------------------");
+    for (const station of stations) {
+      const stationURL = `https://www.edsm.net/en/system/stations/id/starSystem.id/name/${system.name}/details/idS/${station.id}/`;
+      let faction = "No Faction";
+      const distance = Math.round(station.distanceToArrival * 10) / 10;
+      if (station.controllingFaction) {
+        faction = station.controllingFaction.name;
+      }
+      embed.addField(faction, "[" + station.name + "](" + encodeURI(stationURL) + ")\n" + distance + " ls", true);
+    }
+  }
+
+  // Letting the user know there were more than 25
+  if (stationList.size > 25) embed.setFooter({ text: "Some stations were filtered out because the limit was exceeded.", iconURL: "https://i.imgur.com/vYPj8iX.png" });
+  return { embeds: [embed] };
+}
+
+/**
+ * @param {string} system
+ * @param {discord.MessageEmbed} embed
+ */
+async function eliteGetFactions(system, embed) {
+  if (system.factions.length < 1) return { content: "I couldn't find any factions in that system.", ephemeral: true };
+  embed.setTitle(system.name).setURL(system.factionsURL);
+
+  for (const faction of system.factions) {
+    const influence = Math.round(faction.influence * 10000) / 100;
+    const url = encodeURI(`https://www.edsm.net/en/faction/id/${faction.id}/name/`);
+    embed.addField(faction.name + (system.information && (faction.name === system.information.faction) ? " (Controlling)" : "") + " " + influence + "%",
+      "State: " + faction.state + "\nGovernment: " + faction.allegiance + " - " + faction.government + "\n[Link](" + url + ")", true);
+  }
+  return { embeds: [embed] };
+}
+
+/**
+ * @param {string} system
+ * @param {discord.MessageEmbed} embed
+ */
+async function eliteGetBodies(system, embed) {
+  if (system.bodies.length < 1) return { content: "I couldn't find any bodies in that system.", ephemeral: true };
+  embed.setTitle(system.name).setURL(system.bodiesURL);
+
+  for (const body of system.bodies) {
+    const scoopable = body.type === "Star" ? (body.isScoopable ? " (Scoopable)" : " (Not Scoopable)") : "";
+    const distance = Math.round(body.distanceToArrival * 10) / 10;
+    embed.addField(body.name, body.type + scoopable + "\n" + distance + " ls", true);
+  }
+  return { embeds: [embed] };
+}
+
 Module.addInteraction({
   name: "game",
   guildId: u.sf.ldsg,
@@ -151,9 +356,9 @@ Module.addInteraction({
       switch (interaction.options.getSubcommand()) {
       case "chess": return slashGameChess(interaction);
       case "destiny": return slashGameDestiny(interaction);
-      // case "elite": return slashGameElite(inter);
-      // case "minecraft-skin": return slashGameMinecraftSkin(inter);
-      // case "playing": return slashGameGetPlaying(inter);
+      case "elite": return slashGameElite(interaction);
+      case "minecraft-skin": return slashGameMinecraftSkin(interaction);
+      case "playing": return slashGameGetPlaying(interaction);
       default:
         u.errorHandler(Error("Unknown interaction command."), interaction);
       }
--- registry/slashGame.js
+++ registry/slashGame.js
@@ -71,10 +71,77 @@ const destiny = new u.sub()
       .setDescription("Set to true if you want to remove them from the clan/role")
   );
 
+const minecraft = new u.sub()
+  .setName("minecraft-skin")
+  .setDescription("Get a picture of someone's Minecraft skin")
+  .addUserOption(
+    new u.user()
+      .setName("user")
+      .setDescription("The Discord user to search for")
+  )
+  .addStringOption(
+    new u.string()
+      .setName("username")
+      .setDescription("The username of the Minecraft player")
+  );
+
+const elite = new u.sub()
+  .setName("elite")
+  .setDescription("Elite: Dangerous information")
+  .addStringOption(
+    new u.string()
+      .setName("info")
+      .setDescription("The information to get")
+      .addChoices(
+        {
+          "name": "Status",
+          "value": "status"
+        },
+        {
+          "name": "Time",
+          "value": "time"
+        },
+        {
+          "name": "System",
+          "value": "system"
+        },
+        {
+          "name": "Factions",
+          "value": "factions"
+        },
+        {
+          "name": "Bodies",
+          "value": "bodies"
+        },
+        {
+          "name": "Stations",
+          "value": "stations"
+        }
+      )
+      .setRequired(true)
+  )
+  .addStringOption(
+    new u.string()
+      .setName("system-name")
+      .setDescription("The name of the system (needed for all but time and status)")
+  );
+
+const playing = new u.sub()
+    .setName("playing")
+    .setDescription("Find who is playing a game in the server, or list all games being played.")
+    .addStringOption(
+      new u.string()
+        .setName("game")
+        .setDescription("The game to search for (leave blank for a list of all games being played)")
+    );
+
 module.exports = new u.cmd()
   .setName("game")
   .setDescription("Get information on games")
   .setDMPermission(false)
   .addSubcommand(chess)
   .addSubcommand(destiny)
+  .addSubcommand(minecraft)
+  .addSubcommand(playing)
+  .addSubcommand(elite)
   .toJSON();
