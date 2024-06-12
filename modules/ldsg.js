e272fe93bcf4f76b3e1926b0385b1769d61e54fe
LDSGOhGo <56556525+LDSGOhGo@users.noreply.github.com>
2024-06-11 19:40:13 -0600

    fixes yippie
    suggested changes complete

--- modules/ldsg.js
+++ modules/ldsg.js
@@ -21,20 +21,20 @@ async function slashLdsgMembers(interaction) {
 
 const sendOptions = [
   u.MessageActionRow().setComponents([
-    new u.Button().setCustomId("pa").setEmoji("🗣️").setLabel("Public Affairs").setStyle(ButtonStyle.Success),
-    new u.Button().setCustomId("log").setEmoji("🚚").setLabel("Logistics").setStyle(ButtonStyle.Success),
-    new u.Button().setCustomId("ops").setEmoji("🗓️").setLabel("Operations").setStyle(ButtonStyle.Success),
-    new u.Button().setCustomId("mgmt").setEmoji("⚙️").setLabel("Management").setStyle(ButtonStyle.Primary),
-    new u.Button().setCustomId("icarus").setEmoji("🤖").setLabel("Icarus").setStyle(ButtonStyle.Primary),
+    new u.Button().setCustomId("suggestionPA").setEmoji("🗣️").setLabel("Public Affairs").setStyle(ButtonStyle.Success),
+    new u.Button().setCustomId("suggestionLog").setEmoji("🚚").setLabel("Logistics").setStyle(ButtonStyle.Success),
+    new u.Button().setCustomId("suggestionOps").setEmoji("🗓️").setLabel("Operations").setStyle(ButtonStyle.Success),
+    new u.Button().setCustomId("suggestionMgmt").setEmoji("⚙️").setLabel("Management").setStyle(ButtonStyle.Primary),
+    new u.Button().setCustomId("suggestionIcarus").setEmoji("🤖").setLabel("Icarus").setStyle(ButtonStyle.Primary),
   ]),
   u.MessageActionRow().setComponents([
-    new u.Button().setCustomId("ignore").setEmoji("⚠️").setLabel("Ignore").setStyle(ButtonStyle.Danger),
+    new u.Button().setCustomId("suggestionIgnore").setEmoji("⚠️").setLabel("Ignore").setStyle(ButtonStyle.Danger),
   ])
 ];
 
 const replyOption = [
   u.MessageActionRow().setComponents([
-    new u.Button().setCustomId("reply").setEmoji("🗨️").setLabel("Reply to user").setStyle(ButtonStyle.Primary)
+    new u.Button().setCustomId("suggestionReply").setEmoji("🗨️").setLabel("Reply to user").setStyle(ButtonStyle.Primary)
   ])
 ];
 
@@ -43,7 +43,8 @@ async function slashLdsgSuggest(int) {
   const suggestion = int.options.getString("suggestion", true);
   await int.deferReply({ ephemeral: true });
   const embed = u.embed({ author: int.user })
-    .addFields({ name: "User ID:", value: int.user.id }, { name: "Suggestion:", value: suggestion });
+    .addFields({ name: "Suggestion:", value: suggestion })
+    .setFooter({ text: `User ID: ${int.user.id}` });
   await int.client.getTextChannel(u.sf.channels.suggestionBox)?.send({ embeds: [embed], components: sendOptions });
   int.editReply("Sent!");
   return int.user.send({ content: "Sent:", embeds: [embed] });
@@ -61,65 +62,53 @@ async function processCardAction(int) {
           .setLabel("Reply")
           .setStyle(Discord.TextInputStyle.Short)
           .setRequired(true)
-          .setPlaceholder("Your reply for the user")
+          .setPlaceholder("Your reply to the user")
       ])
-    ).setCustomId("suggestionReply").setTitle("Suggestion Reply");
+    ).setCustomId("reply").setTitle("Suggestion Reply");
     let submitted;
     let em;
     let reply;
     let member;
-    /** @type {String} */
-    let channel;
-    switch (int.customId) {
-      case "pa":
-        channel = u.sf.channels.publicaffairsForum;
-        break;
-      case "log":
-        channel = u.sf.channels.logisticsForum;
-        break;
-      case "ops":
-        channel = u.sf.channels.operationsForum;
-        break;
-      case "icarus":
-        channel = u.sf.channels.bottestingForums;
-        break;
-      case "mgmt":
-        channel = u.sf.channels.managementForum;
-        break;
-      case "reply":
-        await int.showModal(modal);
-        submitted = await int.awaitModalSubmit({ time: 5 * 60 * 1000, dispose: true }).catch(() => {
-          return null;
-        });
-        if (!submitted) {
-          return int.channel?.send("I fell asleep waiting for your input...");
-        }
-        await submitted.deferUpdate();
-        reply = submitted.fields.getTextInputValue("text");
-        em = u.embed({ author: int.user })
+    const channel = {
+      suggestionPA: u.sf.channels.publicaffairsForum,
+      suggestionLog: u.sf.channels.logisticsForum,
+      suggestionOps: u.sf.channels.operationsForum,
+      suggestionMgmt: u.sf.channels.managementForum,
+      suggestionIcarus: u.sf.channels.bottestingForum
+    }[int.customId];
+    if (channel) {
+      embed.addFields({ name: `Sent to <#${channel}>`, value: `by ${int.user}` });
+      int.client.getForumChannel(channel)?.threads.create({ name: `Suggestion from ${suggestion.embeds[0].author?.name}`, message: { embeds: [suggestion.embeds[0]], components: replyOption } });
+      return int.update({ embeds: [embed], components: [] });
+    } else if (int.customId == "suggestionReply") {
+      await int.showModal(modal);
+      submitted = await int.awaitModalSubmit({ time: 5 * 60 * 1000, dispose: true }).catch(() => {
+        return null;
+      });
+      if (!submitted) {
+        return int.channel?.send("I fell asleep waiting for your input...");
+      }
+      await submitted.deferUpdate();
+      reply = submitted.fields.getTextInputValue("text");
+      em = u.embed({ author: int.user })
           .setTitle("Suggestion feedback")
           .addFields({ name: "Reply:", value: reply });
-        member = int.guild.members.cache.get(suggestion.embeds[0].fields.find(field => field.name === "User ID:")?.value ?? "");
-        if (!member) {
-          return int.channel?.send("I could not find that member");
-        }
-        try {
-          member.send({ embeds: [em] });
-          return int.channel?.send({ content: "Replied to user:", embeds: [em] });
-        } catch (e) {
-          u.errorHandler(e, int);
-          return int.channel?.send("Failed to message member, they may have me blocked. You will need to reach out to them on your own this time!");
-        }
-      case "ignore":
-        embed.addFields({ name: `Suggestion ignored`, value: `by ${int.user}` });
-        return int.update({ embeds: [embed], components: [] });
-      default:
-        channel = "Channel not found";
+      member = int.guild.members.cache.get(suggestion.embeds[0].footer?.text.slice(9) ?? "");
+      if (!member) {
+        return int.channel?.send("I could not find that member");
+      }
+      try {
+        member.send({ embeds: [em] });
+        return int.channel?.send({ content: "Replied to user:", embeds: [em] });
+      } catch (e) {
+        u.errorHandler(e, int);
+        return int.channel?.send("Failed to message member, they may have me blocked. You will need to reach out to them on your own this time!");
+      }
+    } else {
+      embed.addFields({ name: `Suggestion ignored`, value: `by ${int.user}` });
+      return int.update({ embeds: [embed], components: [] });
     }
-    embed.addFields({ name: `Sent to <#${channel}>`, value: `by ${int.user}` });
-    int.client.getForumChannel(channel)?.threads.create({ name: `Suggestion from ${suggestion.embeds[0].author?.name}`, message: { embeds: [suggestion.embeds[0]], components: replyOption } });
-    return int.update({ embeds: [embed], components: [] });
-  } catch (e) { u.errorHandler(e, int); }
+  } catch (e) { u.noop; }
 }
 
 const Module = new Augur.Module()
@@ -139,7 +128,7 @@ const Module = new Augur.Module()
     if (!u.perms.calc(int.member, ["team"])) {
       return int.reply({ content: "You don't have permissions to interact with this suggestion!", ephemeral: true });
     }
-    if (['pa', 'log', 'ops', 'icarus', 'reply', 'mgmt', 'ignore']
+    if (['suggestionPA', 'suggestionLog', 'suggestionOps', 'suggestionIcarus', 'suggestionReply', 'suggestionMgmt', 'suggestionIgnore']
       .includes(int.customId)) return processCardAction(int);
   });
 
