19333cce308a891d0e5245aae45041d94121089a
j-romchain <Jaromrightdragon@gmail.com>
2024-08-29 00:47:06 -0400

    but it could just be a tag.

--- registry/slashFun.js
+++ registry/slashFun.js
@@ -75,9 +75,6 @@ const repost = new u.sub()
 const acronym = new u.sub()
 .setName("acronym")
 .setDescription("Get a random 3-5 letter acronym. For science.");
-const chaos = new u.sub()
-.setName("chaos")
-.setDescription("You just walked into chaos.");
 const mines = new u.sub()
   .setName("mines")
   .setDescription("Play a game of Minesweeper!")
@@ -163,5 +160,4 @@ module.exports = new u.cmd()
   .addSubcommand(buttermelon)
   .addSubcommand(quote)
   .addSubcommand(nameGame)
-  .addSubcommand(chaos)
   .toJSON();
