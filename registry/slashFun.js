b03d6ec3520c3523d3cfb98a4a12509e6a93b470
j-romchain <Jaromrightdragon@gmail.com>
2024-08-29 23:49:30 -0400

    I forgor

--- registry/slashFun.js
+++ registry/slashFun.js
@@ -26,31 +26,6 @@ const roll = new u.sub()
     .setDescription("How much to change the roll by? (defaults to 0)")
     .setRequired(false)
   );
-const rollF = new u.sub()
-  .setName("rollf")
-  .setDescription("Roll Fate Dice")
-  .addIntegerOption(
-    new u.int()
-    .setName("dice")
-    .setDescription("How many dice to roll? (will roll at least 1 anyway) (max of 10000)")
-    .setRequired(false)
-    .setMinValue(1)
-    .setMaxValue(10000)
-  )
-  .addIntegerOption(
-    new u.int()
-    .setName("modifier")
-    .setDescription("How much to change the roll by? (defaults to 0)")
-    .setRequired(false)
-  );
-const rollOld = new u.sub()
-  .setName("rollold")
-  .setDescription("Roll Dice using the old formula format")
-  .addStringOption(
-    new u.string()
-    .setName("rollformula")
-    .setDescription("old dice formula to use, it errors in the same way as !roll did")
-  );
 const ball8 = new u.sub()
   .setName("8ball")
   .setDescription("Get an answer from the Magic 8-ball.")
