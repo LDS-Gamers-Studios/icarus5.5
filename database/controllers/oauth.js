624c8e2c35f7c945c5240e85b2a501f76dacae37
BobbyTheCatfish <46359040+BobbyTheCatfish@users.noreply.github.com>
2024-09-17 15:18:58 -0600

    tscheck test initial and code fixing

--- database/controllers/oauth.js
+++ database/controllers/oauth.js
@@ -3,9 +3,15 @@
 const OAuth = require("../models/Infraction.model");
 
 module.exports = {
+  /** @param {string} id */
   get: (id) => {
     return OAuth.findOne({ id }, undefined, { lean: true });
   },
+  /**
+   * @param {string} id
+   * @param {string} access
+   * @param {string} refresh
+   */
   update: (id, access, refresh) => {
     return OAuth.findOneAndUpdate({ id }, { accessToken: access, refreshToken: refresh }, { lean: true, upsert: true });
   }
