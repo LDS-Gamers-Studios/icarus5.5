5ba7f27de99235ef09ccded10bca5930e77e0fa1
BobbyTheCatfish <46359040+BobbyTheCatfish@users.noreply.github.com>
2024-09-28 16:14:22 -0400

    bobby insight
    changed jimp to Jimp since its a class
    simplified some var declarations
    slothmare emoji instead of sloth
    better wording and layout for hbs results
    better roll result string
    no if () {return} else {}
    test to dms
    noop instead of errorhandler on *buttermelons
    better formatting for /quote
    simpler namegame name var declaration
    simpler song extraction
    general text cleanup

--- registry/slashFun.js
+++ registry/slashFun.js
@@ -3,88 +3,95 @@ const u = require('./regUtils');
 
 const roll = new u.sub()
   .setName("roll")
-  .setDescription("Roll Dice")
+  .setDescription("Roll some dice!")
   .addIntegerOption(
     new u.int()
-    .setName("dice")
-    .setDescription("How many dice to roll? (Default: 1)")
-    .setRequired(false)
-    .setMinValue(1)
-    .setMaxValue(10000)
+      .setName("dice")
+      .setDescription("How many dice to roll? (Default: 1)")
+      .setRequired(false)
+      .setMinValue(1)
+      .setMaxValue(10000)
   )
   .addIntegerOption(
     new u.int()
-    .setName("sides")
-    .setDescription("How many sides on the dice? (Default: 6)")
-    .setRequired(false)
-    .setMinValue(1)
-    .setMaxValue(100000)
+      .setName("sides")
+      .setDescription("How many sides on the dice? (Default: 6)")
+      .setRequired(false)
+      .setMinValue(1)
+      .setMaxValue(100000)
   )
   .addIntegerOption(
     new u.int()
-    .setName("modifier")
-    .setDescription("How much to change the roll by. (Default: 0)")
-    .setRequired(false)
+      .setName("modifier")
+      .setDescription("How much to change the roll by. (Default: 0)")
+      .setRequired(false)
   );
+
 const ball8 = new u.sub()
   .setName("8ball")
   .setDescription("Get an answer from the Magic 8-ball.")
   .addStringOption(
     new u.string()
-    .setName("question")
-    .setDescription("What do you wish to ask the 8-ball today?")
-    .setRequired(true)
+      .setName("question")
+      .setDescription("What do you wish to ask the 8-ball today?")
+      .setRequired(true)
   );
+
 const choose = new u.sub()
   .setName("choose")
   .setDescription("Helps make a choice!")
   .addStringOption(
     new u.string()
-    .setName("options")
-    .setDescription('options to choose from separated by "|"')
-    .setRequired(true)
+      .setName("options")
+      .setDescription('Options to choose from separated by "|"')
+      .setRequired(true)
   );
+
 const emoji = new u.sub()
   .setName("emoji")
-  .setDescription("combines two (non-custom) emoji")
+  .setDescription("Combines two (non-custom) emoji")
   .addStringOption(
     new u.string()
-    .setName("emoji1")
-    .setDescription("first emoji to combine")
-    .setRequired(true)
+      .setName("emoji1")
+      .setDescription("First emoji to combine")
+      .setRequired(true)
   )
   .addStringOption(
     new u.string()
-    .setName("emoji2")
-    .setDescription("second emoji to combine")
-    .setRequired(true)
+      .setName("emoji2")
+      .setDescription("Second emoji to combine")
+      .setRequired(true)
   );
+
 const repost = new u.sub()
-.setName("repost")
-.setDescription("That's a repost.");
+  .setName("repost")
+  .setDescription("That's a repost!");
+
 const acronym = new u.sub()
-.setName("acronym")
-.setDescription("Get a random acronym. For science.")
-.addIntegerOption(
-  new u.int()
-  .setName("length")
-  .setDescription("How long of an acronym?")
-  .setRequired(false)
-  .setMinValue(1)
-  .setMaxValue(10)
-);
+  .setName("acronym")
+  .setDescription("Get a random acronym. For science.")
+  .addIntegerOption(
+    new u.int()
+      .setName("length")
+      .setDescription("How long of an acronym?")
+      .setRequired(false)
+      .setMinValue(1)
+      .setMaxValue(10)
+  );
+
 const mines = new u.sub()
   .setName("mines")
   .setDescription("Play a game of Minesweeper!")
   .addStringOption(
     new u.string()
-    .setName("difficulty")
-    .setDescription("5 by 5 with 5 mines, 10 by 10 with 30 mines, or 10 by 18 with 60 mines")
-    .setRequired(true)
-    .setChoices(
-      { name: "Easy", value: "Easy" },
-      { name: "Medium", value: "Medium" },
-      { name: "Hard", value: "Hard" })
+      .setName("difficulty")
+      .setDescription("5 by 5 with 5 mines, 10 by 10 with 30 mines, or 10 by 18 with 60 mines")
+      .setRequired(true)
+      .setChoices(
+        { name: "Easy", value: "Easy" },
+        { name: "Medium", value: "Medium" },
+        { name: "Hard", value: "Hard" }
+      )
   )
   .addIntegerOption(
     new u.int()
@@ -112,27 +119,29 @@ const mines = new u.sub()
     .setDescription("override the preopened spaces in the minefeild.")
     .setMinValue(0)
   );
+
 const hbs = new u.sub()
   .setName("hbs")
   .setDescription("Play a game of Handicorn, Buttermelon, Sloth!")
   .addStringOption(
     new u.string()
-    .setName("choice")
-    .setDescription("Your choice of Handicorn, Buttermelon, or Sloth!")
-    .setRequired(true)
-    .setChoices(
-      { name: "Handicorn", value: "Handicorn" },
-      { name: "Buttermelon", value: "Buttermelon" },
-      { name: "Sloth", value: "Sloth" })
+      .setName("choice")
+      .setDescription("Your choice of Handicorn, Buttermelon, or Sloth!")
+      .setRequired(true)
+      .setChoices(
+        { name: "Handicorn", value: "Handicorn" },
+        { name: "Buttermelon", value: "Buttermelon" },
+        { name: "Sloth", value: "Sloth" }
+      )
   )
   .addStringOption(
     new u.string()
-    .setName("mode")
-    .setDescription("Who do you want to play against?")
-    // .setRequired(false)
-    .setChoices(
-      { name: "Icarus", value: "icarus" },
-      { name: "Other Users", value: "user" })
+      .setName("mode")
+      .setDescription("Who do you want to play against?")
+      .setChoices(
+        { name: "Other Users", value: "user" },
+        { name: "Icarus", value: "icarus" }
+      )
   );
 
 const color = new u.sub()
@@ -140,23 +149,27 @@ const color = new u.sub()
   .setDescription("Show what a color looks like.")
   .addStringOption(
     new u.string()
-    .setName("color")
-    .setDescription("color (e.g. `#003B6F` or `blue`)")
+      .setName("color")
+      .setDescription("color (e.g. '#003B6F' or 'blue')")
   );
+
 const nameGame = new u.sub()
   .setName("namegame")
   .setDescription("Sing the Name Game!")
   .addStringOption(
     new u.string()
-    .setName("name")
-    .setDescription("The name to play with (Default: your username)")
+      .setName("name")
+      .setDescription("The name to play with (Default: your username)")
   );
+
 const quote = new u.sub()
   .setName("quote")
   .setDescription("Get a random quote!");
+
 const buttermelon = new u.sub()
   .setName("buttermelon")
   .setDescription("Get a random buttermelon fact");
+
 module.exports = new u.cmd()
   .setName("fun")
   .setDescription("Its all fun and games till someone gets banned.")
