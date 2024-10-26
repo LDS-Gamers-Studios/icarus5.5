5efc3be6643e58378719a571054a7af3eebfccf0
j-romchain <57956651+j-romchain@users.noreply.github.com>
2024-10-26 17:26:42 -0400

    migrate fun.js, dice.js, and buttermelon.js (#123)
    * add /fun repost and /fun rollf and /fun rollold
    
    * fixed a bunch of bugs. may be worthy of a pull request at this point.
    
    * added a bunch of commands tested on stephenbot, just a few more to go before its on to cleanup and pull request.
    
    * cleanup and /hug. perhaps more cleanup, but I think we may be ready to pr.
    
    * add javadoc
    
    * forgot to add the fun
    
    * ready for PR
    
    * add buttermelon
    
    * noticed some slightly broken doc
    
    * add name game... this added some dependancies.
    
    * /fun quote done as well, there should be nothing else in need of porting to /fun.
    
    * get ready to make the full thing, make a branch, and then trim down for merging with icarus, and then save the rest for adding in later if possible. I totally not a data/code hoarder... definitely not...
    
    * remove dependancies I added to prepare to move to axios... this could take a while to figure out.
    
    * finish the migration of those to axios, and removal of cheerios
    
    * rework the logic for hbs
    
    * added length parameter to acronym
    
    * use ``${variable}` when possible
    
    * add some configs for registering commands and for testing on linux
    
    * If a function only needs to be called once...
    
    * AllThe Chaos Hugs are Gone
    
    * the service that was being used was shut down, so I found another one and implemented it much more cleanly, albeit with much less freedom. just 2 emoji now, and only default emoji, but it also uses google kitchen to handle up to ?100000? unique combonations.
    
    * make repost handle videos and other wonky stuff and make it repost the latest instead of the oldest.
    
    * invalid color handling
    
    * applying some stuff in minesweeper from catfish
    
    * update gitignore to make it ignore intellij configs until some good ones are made
    
    * fix some stuff with the mines command
    
    * working on the emoji command to make it scrape gstatic instead of some random vercel app I found.
    
    * bobby insight
    
    changed jimp to Jimp since its a class
    simplified some var declarations
    slothmare emoji instead of sloth
    better wording and layout for hbs results
    better roll result string
    no if () {return} else {}
    test to dms
    noop instead of errorhandler on *buttermelons
    better formatting for /quote
    simpler namegame name var declaration
    simpler song extraction
    general text cleanup
    
    * bobby stuff p2
    
    fix /fun color
    eslint rule
    changed api location
    slight wording fix
    unload and reload stored options for hbs
    
    * minesadvanced and ts hates me.
    
    * remove duplicate facts
    
    * cleanup
    
    fixing text
    deleting error replies
    hbs simplification
    more comments
    removing console logs
    ensure minesweeper messages send in order
    axios instead of fetch
    disable mines-advanced for now (at least for the initial drop. we may turn it back on later)
    
    * more text cleanup
    
    ---------
    
    Co-authored-by: j-romchain <Jaromrightdragon@gmail.com>
    Co-authored-by: BobbyTheCatfish <46359040+BobbyTheCatfish@users.noreply.github.com>

---
+++ modules/fun.js
@@ -0,0 +1,484 @@
+// @ts-check
+const Augur = require("augurbot-ts"),
+  Discord = require("discord.js"),
+  config = require("../config/config.json"),
+  u = require("../utils/utils"),
+  axios = require('axios'),
+  Jimp = require('jimp'),
+  profanityFilter = require("profanity-matcher"),
+  buttermelonFacts = require('../data/buttermelonFacts.json'),
+  emojiKitchenSpecialCodes = require("../data/emojiKitchenSpecialCodes.json"),
+  emojilib = require('node-emoji'),
+  mineSweeperEmojis = ['0⃣', '1⃣', '2⃣', '3⃣', '4⃣', '5⃣', '6⃣', '7⃣', '8⃣', '💣'];
+
+/** @param {Discord.ChatInputCommandInteraction} int */
+async function slashFunColor(int) {
+  // get input or random color
+  const colorCode = int.options.getString("color") || "#" + Math.floor(Math.random() * 16777216).toString(16).padStart(6, '0');
+  // In the case that we have a string in 0xABCDEF format
+  /** @type {string | number} */
+  let colorCSS = colorCode.replace('0x', "#");
+  try {
+    // make sure it is a valid color, and not just defaulting to black
+    if (!["#000000", "black", "#000000FF"].includes(colorCSS)) colorCSS = Jimp.cssColorToHex(colorCSS);
+    if (colorCSS === 255) {
+      return int.reply({ content: `Sorry, I couldn't understand the color \`${colorCode}\``, ephemeral: true });
+    }
+    await int.deferReply();
+    // make and send the image
+    const img = new Jimp(256, 256, colorCSS);
+    return int.editReply({ files: [await img.getBufferAsync(Jimp.MIME_JPEG)] });
+  } catch (error) {
+    const content = `Sorry, I couldn't understand the color \`${colorCode}\``;
+    if (int.replied || int.deferred) return int.editReply({ content }).then(u.clean);
+    int.reply({ content, ephemeral: true });
+  }
+}
+
+// global hbs stuff
+const hbsValues = {
+  'Buttermelon': { emoji: `<:buttermelon:${u.sf.emoji.buttermelon}>`, beats: "Handicorn", looses: "Sloth" },
+  'Handicorn': { emoji: `<:handicorn:${u.sf.emoji.handicorn}>`, beats: "Sloth", looses: "Buttermelon" },
+  'Sloth': { emoji: `<:slothmare:${u.sf.emoji.slothmare}>`, beats: "Buttermelon", looses: "Handicorn" }
+};
+let storedChooser = '';
+let storedChoice = '';
+
+/** @param {Discord.ChatInputCommandInteraction} int */
+async function slashFunHBS(int) {
+  const mode = int.options.getString("mode") || "user";
+  const choice = int.options.getString("choice", true);
+  const chooser = int.user.toString();
+  const botLobby = int.client.getTextChannel(u.sf.channels.botspam);
+  let challenged;
+  if (mode === "user") {
+    if (!storedChoice) {
+      storedChooser = chooser;
+      storedChoice = choice;
+      int.reply({ content: `Your fighter has been picked! ${int.channelId !== u.sf.channels.botspam ? `Check ${botLobby} to see the results!` : ""}`, ephemeral: true });
+      return botLobby?.send("## Handicorn, Buttermelon, Sloth, Fight!\n" +
+      `${chooser} has chosen their fighter and is awaiting a challenger.`);
+    } else if (storedChooser === chooser) {
+      storedChoice = choice;
+      int.reply({ content: `Your fighter has been updated! ${int.channelId !== u.sf.channels.botspam ? `Check ${botLobby} to see the results!` : ""}`, ephemeral: true });
+      return botLobby?.send("## Handicorn, Buttermelon, Sloth, Fight!\n" +
+      `${chooser} has changed their fighter and is awaiting a challenger.`
+      );
+    }
+
+    challenged = { user: storedChooser, choice: storedChoice };
+    // reset stored values
+    storedChooser = '';
+    storedChoice = '';
+  } else {
+    challenged = { user: int.client.user.toString(), choice: u.rand(Object.keys(hbsValues)) };
+  }
+  return int.reply({ content: "## Handicorn, Buttermelon, Sloth, Fight!\n" +
+      `🥊 ${chooser} challenged ${challenged.user}!\n` +
+      hbsResult(chooser, choice, challenged.user, challenged.choice),
+  allowedMentions: { parse: ["users"] } });
+}
+
+/**
+ * function hbsResult
+ * @param {string} chooser1 a string to represent who made choice 1
+ * @param {string} choice1 chooser1's "Handicorn", "Buttermelon", or "Sloth" choice
+ * @param {string} chooser2 ...
+ * @param {string} choice2 ...
+ * @return {string} a summary including who picked what and who won.
+ */
+function hbsResult(chooser1, choice1, chooser2, choice2) {
+  let response = `🤼 ${chooser1} picked ${hbsValues[choice1].emoji}, ${chooser2} picked ${hbsValues[choice2].emoji}.\n### `;
+  if (choice1 === choice2) {
+    response += "🤝 It's a tie!";
+  } else if (hbsValues[choice1].beats === choice2) {
+    response += `🏆 ${chooser1} wins!`;
+  } else {
+    response += `😵‍💫 ${chooser1} looses!`;
+  }
+  return response;
+}
+
+/** @param {Discord.ChatInputCommandInteraction} int */
+async function slashFunAcronym(int) {
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+  // input or number between 3 and 5
+  const len = int.options.getInteger("length") || Math.floor(Math.random() * 3) + 3;
+  const pf = new profanityFilter();
+  let wordgen = [];
+
+  // try a bunch of times
+  for (let ignored = 0; ignored < len * len; ignored++) {
+    // make an acronym
+    for (let i = 0; i < len; i++) {
+      wordgen.push(alphabet[Math.floor(Math.random() * alphabet.length)]);
+    }
+    const word = wordgen.join("");
+    // see if it has any bad words
+    if (pf.scan(word.toLowerCase()).length === 0) {
+      return int.reply(`I've always wondered what __**${word}**__ stood for...`);
+    }
+    wordgen = [];
+
+  }
+  return int.reply("I've always wondered what __**IDUTR**__ stood for...");// cannonically it hearby stands for "IDiUT eRror"
+}
+
+/** @param {Discord.ChatInputCommandInteraction} int */
+async function slashFunMinesweeper(int) {
+  let edgesize, mineCount, preclickCount;
+  switch (int.options.getString("difficulty", true)) {
+    case "Hard":
+      edgesize = [10, 18];
+      mineCount = 60;
+      preclickCount = 6;
+      break;
+    case "Medium":
+      edgesize = [10, 10];
+      mineCount = 30;
+      preclickCount = 4;
+      break;
+    default:
+      edgesize = [5, 5];
+      mineCount = 5;
+      preclickCount = 4;
+      break;
+  }
+  // override with manual numbers if given
+  edgesize[0] = int.options.getInteger("width") || edgesize[0];
+  edgesize[1] = int.options.getInteger("height") || edgesize[1];
+  mineCount = int.options.getInteger("minecount") || mineCount;
+  preclickCount = int.options.getInteger("preclickcount") || preclickCount;
+  // x and y lengths (for custom dimensions)
+  const [width, height] = edgesize;
+  preclickCount = Math.min(width * height, preclickCount);
+  mineCount = Math.min(width * height - preclickCount, mineCount);
+  // Create a 2d array for the board
+  const board = new Array(height).fill([]).map(() => {return new Array(width).fill(0);});
+  // Convert the 2d array to a 2d index array
+  const spaces = board.map((r, y) => {
+    const row = new Array(width + 1);
+    row[0] = y;
+    r.forEach((_, x) => {row[x + 1] = x;});
+    return row;
+  });
+  // place mines
+  for (let i = 0; i < mineCount; i++) {
+    // Get a random position
+    const rownum = Math.floor(Math.random() * spaces.length);
+    const row = spaces[rownum];
+    const y = row[0];
+    const slotnum = Math.floor(Math.random() * (row.length - 1)) + 1;
+    const x = row[slotnum];
+    // Set the value to a mine
+    board[y][x] = 9;
+    // Remove from possible mine spaces
+    row.splice(slotnum, 1);
+    if (row.length === 1) {
+      spaces.splice(rownum, 1);
+    }
+    // Increment all spots around it
+    for (let incrementx = Math.max(0, x - 1); incrementx < Math.min(width, x + 2); incrementx++) {
+      for (let incrementy = Math.max(0, y - 1); incrementy < Math.min(height, y + 2); incrementy++) {
+        board[incrementy][incrementx]++;
+      }
+    }
+  }
+
+  for (let i = 0; i < preclickCount; i++) {
+    // Get a random position
+    const rownum = Math.floor(Math.random() * spaces.length);
+    const row = spaces[rownum];
+    const y = row[0];
+    const slotnum = Math.floor(Math.random() * (row.length - 1)) + 1;
+    const x = row[slotnum];
+    // expose it
+    board[y][x] = -1 - board[y][x];
+    // Remove from non-special-spaces
+    row.splice(slotnum, 1);
+    if (row.length === 1) {
+      spaces.splice(rownum, 1);
+    }
+  }
+  // seperate into rows and emojify and hide if not exposed
+  const rowStrings = board.map(row => row.map(num => num < 0 ? mineSweeperEmojis[-num - 1] : `||${mineSweeperEmojis[Math.min(num, 9)]}||`).join(""));
+  if (!int.channel) {
+    return int.reply({ content: `I can't figure out where to put the board in here, try again in another channel like <#${u.sf.channels.botspam}>`, ephemeral: true });
+  }
+  await int.reply(`**Mines: ${mineCount}**`);
+  const messages = [""];
+  let messageCount = 0;
+  let tagpairs = 0;
+  // max of 200 spoiler tags per message, split into as many as needed
+  rowStrings.forEach((row) => {
+    if (tagpairs + (width * 2) > 199) {
+      tagpairs = 0;
+      messageCount++;
+      messages[messageCount] = "";
+    }
+    tagpairs += width * 2;
+    messages[messageCount] += row + "\n";
+  });
+  // send the messages in order
+  let i = 0;
+  do {
+    const msg = messages[i];
+    await int.channel.send(msg);
+    i++;
+  } while (i < messages.length);
+}
+
+/** @param {Discord.ChatInputCommandInteraction} int */
+async function slashFunRoll(int) {
+  // get inputs
+  const dice = int.options.getInteger('dice') || 1;
+  const sides = int.options.getInteger('sides') || 6;
+  const modifier = int.options.getInteger('modifier') || 0;
+  if (dice > 10000) {
+    return int.reply({ content: "I'm not going to roll *that* many dice... 🙄", ephemeral: true });
+  }
+  // calculate rolls
+  /** @type {number[]} */
+  const rolls = [];
+  for (let i = 0; i < dice; i++) {
+    rolls.push(Math.ceil(Math.random() * sides));
+  }
+  // make it visually pleasing
+  const total = rolls.reduce((p, c) => p + c, 0) + modifier;
+  let rollStr = "";
+  const maxShown = 20;
+  if (rolls.length > maxShown + 3) {
+    const extra = rolls.length - maxShown;
+    const reduced = rolls.filter((r, i) => i < maxShown);
+    rollStr = `${reduced.join(" + ")}, + ${extra} more`;
+  } else {
+    rollStr = rolls.join(" + ");
+  }
+  const modStr = modifier > 0 ? ` + ${modifier}` : modifier ? ` - ${Math.abs(modifier)}` : "";
+  const summary = `${rollStr ? ` (**1d${sides}**: ${rollStr})` : ""}${modStr ? `**${modStr}**` : ""}`;
+  // send visually pleasing result
+  return int.reply(`You rolled ${dice}d${sides}${modStr} and got \`${total}\`!\n ${summary}`);
+}
+
+/** @param {Discord.ChatInputCommandInteraction} int */
+async function slashFun8ball(int) {
+  const question = int.options.getString("question", true);
+  if (!question.endsWith("?")) {
+    return int.reply({ content: "You need to ask me a question, silly.", ephemeral: true });
+  }
+  const outcomes = [
+    "It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes - definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes.",
+    // the following were removed due to complaints
+    // "Reply hazy, try again.",
+    // "Ask again later.",
+    // "Better not tell you now.",
+    // "Cannot predict now.",
+    // "Concentrate and ask again.",
+    "Don't count on it.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful."
+  ];
+  return int.reply(`❓: \`${question}\`\n` +
+    `🎱: \`${u.rand(outcomes)}\``
+  );
+}
+
+/** @param {Discord.ChatInputCommandInteraction} int */
+async function slashFunRepost(int) {
+  if (!int.channel) {
+    return int.reply({ content: "I don't know where here is, so I can't find anything to repost... try in a more normal channel.", ephemeral: true });
+  }
+  await int.deferReply();
+  const latest = (await int.channel.messages.fetch({ limit: 100 })).filter(m => m.attachments.size > 0 || m.embeds.some(embed => embed.image || embed.video)).first();
+  if (!latest) {
+    return int.editReply("I couldn't find anything in the last 100 messages to repost.").then(u.clean);
+  }
+  return int.editReply({
+    content: 'repost that? ok!',
+    files: latest.attachments.map(a => a.url),
+    embeds: latest.embeds.filter(embed => embed.image || embed.video)
+  });
+}
+
+/** @param {Discord.ChatInputCommandInteraction} int */
+async function slashFunButtermelon(int) {
+  return int.reply(`🍌 ${u.rand(buttermelonFacts)}`);
+}
+
+/** @param {Discord.ChatInputCommandInteraction} int */
+async function slashFunQuote(int) {
+  const url = "https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en";
+  await int.deferReply();
+  const response = await axios({ url, method: "get" }).catch((/** @type {axios.AxiosError} */ e) => {
+    throw new Error(`axios error: ${e.status}\n${e.message}`);
+  });
+  const data = typeof response.data === "string" ? JSON.parse(response.data) : response.data;
+  const embed = u.embed();
+  if (data) {
+    embed.setAuthor({ name: data.quoteAuthor })
+      .setDescription(data.quoteText)
+      .setTimestamp(null);
+  } else {
+    embed.setAuthor({ name: "ChainSword20000" })
+      .setDescription("A developer uses dark mode because bugs are attracted to light, but wouldn't that put the bugs in the code instead of the background?");
+  }
+  return int.editReply({ embeds: [embed] });
+}
+
+/** @param {Discord.ChatInputCommandInteraction} int */
+async function slashFunNamegame(int) {
+  // fun shenanigans (basically check if member is partial (which it probably isnt 99% of the time))
+  const user = int.member && "displayName" in int.member ? int.member.displayName : int.user.displayName;
+  let name = (int.options.getString("name") || user)
+    .replace(/[^a-zA-Z]/g, '_')// just ABCabc etc, numbers were causing problems.
+    .split("_")[0];// and just one segment
+  name = name.charAt(0).toUpperCase() + name.slice(1);
+  try {
+    const url = `https://thenamegame-generator.com/lyrics/${name}.html`;
+    await int.deferReply();
+    // @ts-ignore
+    const response = await axios({ url, method: "get" }).catch(u.noop);
+    if (!response) {
+      return int.editReply(`I couldn't generate lyrics for ${name}.\nPerhaps you can get it yourself from https://thenamegame-generator.com.`).then(u.clean);
+    }
+    // parse the song
+    const song = /<blockquote>\n(.*)<\/blockquote>/g.exec(response?.data)?.[1]?.replace(/<br ?\/>/g, "\n");
+    // make sure its safe
+    const pf = new profanityFilter();
+    const profane = pf.scan(song?.toLowerCase().replace("\n", " ")).length;
+    if (!song) {
+      return int.editReply("I uh... broke my voice box. Try a different name?").then(u.clean);
+    } else if (profane > 0) {
+      return int.editReply("Let's try a different one...").then(u.clean);
+    }
+    const embed = u.embed().setTitle(`🎶 The Name Game! ${name}! 🎵`).setDescription(song);
+    return int.editReply({ embeds: [embed] });
+  } catch (error) { u.errorHandler(error, int); }
+}
+
+/** @param {Discord.ChatInputCommandInteraction} int */
+async function slashFunChoose(int) {
+  const optionsArg = int.options.getString("options", true);
+  if (optionsArg && optionsArg.includes("|")) {
+    const options = optionsArg.split("|");
+    const prefixes = ["I choose", "I pick", "I decided"];
+    return int.reply(`${u.rand(prefixes)} **${u.rand(options).trim()}**`);
+  }
+  return int.reply({ content: 'you need to give me two or more choices! `a | b`', ephemeral: true });
+
+}
+/**
+ * @param {string} emoji unsanitized/irregular emoji input
+ */
+function emojiSanitize(emoji) {
+  let ucode = emojilib.find(emoji)?.emoji ?? emoji;
+  ucode = emojiKitchenSpecialCodes[ucode] ?? ucode;
+  return ucode;
+}
+
+/** @param {Discord.ChatInputCommandInteraction} int */
+async function slashFunEmoji(int) {
+  try {
+    await int.deferReply();
+    const emoji1input = int.options.getString("emoji1", true).trim();
+    const emoji2input = int.options.getString("emoji2", true).trim();
+    const emoji1 = emojiSanitize(emoji1input);
+    const emoji2 = emojiSanitize(emoji2input);
+    const results = await axios(`https://tenor.googleapis.com/v2/featured?key=${config.api.tenor}&client_key=emoji_kitchen_funbox&q=${emoji1}_${emoji2}&collection=emoji_kitchen_v6&contentfilter=high`).catch(u.noop);
+    const url = results?.data?.results[0]?.url;
+    if (url) {
+      return int.editReply({ files: [{ attachment: url, name: "combined.png" }] });
+    }
+    return int.editReply(`For some reason I couldn't combine ${emoji1} and ${emoji2}.`).then(u.clean);
+  } catch (error) {
+    u.errorHandler(error);
+  }
+}
+
+/** @param {Discord.Message|Discord.PartialMessage} msg */
+function buttermelonEdit(msg) {
+  if (msg.channel.isDMBased() && (msg.cleanContent?.toLowerCase() === "test")) {
+    msg.reply((Math.random() < 0.8 ? "pass" : "fail"));
+  }
+  const exclude = [u.sf.channels.minecraftcategory];
+  const roll = Math.random();
+  if (roll < 0.3 && !msg.author?.bot && !exclude.includes(msg.channel.id)) {
+    // let banana = /[bß8ƥɓϐβбБВЬЪвᴮᴯḃḅḇÞ][a@∆æàáâãäåāăȁȃȧɑαдӑӓᴀᴬᵃᵅᶏᶐḁạảấầẩẫậắằẳẵặ4Λ]+([nⁿńňŋƞǹñϰпНhийӣӥѝνṅṇṉṋ]+[a@∆æàáâãäåāăȁȃȧɑαдӑӓᴀᴬᵃᵅᶏᶐḁạảấầẩẫậắằẳẵặ4Λ]+){2}/ig;
+    if (msg.content?.toLowerCase().includes("bananas")) {
+      if (roll < 0.1) {
+        msg.reply({ files: ['media/buttermelonsMan.jpeg'] }).catch(u.noop);
+      } else {
+        msg.reply("*buttermelons").catch(u.noop);
+      }
+    } else if (msg.content?.toLowerCase().includes("banana")) {
+      if (roll < 0.06) {
+        msg.reply({ files: ['media/buttermelonPile.png'] }).catch(u.noop);
+      } else if (roll < 0.1) {
+        msg.reply({ files: ['media/buttermelonMan.jpeg'] }).catch(u.noop);
+      } else {
+        msg.reply("*buttermelon").catch(u.noop);
+      }
+    }
+  }
+}
+
+const Module = new Augur.Module()
+.addInteraction({
+  name: "fun",
+  id: u.sf.commands.slashFun,
+  process: async (int) => {
+    const subcommand = int.options.getSubcommand(true);
+    switch (subcommand) {
+      case "roll": return slashFunRoll(int);
+      case "8ball": return slashFun8ball(int);
+      case "repost": return slashFunRepost(int);
+      case "mines": return slashFunMinesweeper(int);
+      case "minesadvanced": return slashFunMinesweeper(int);
+      case "acronym": return slashFunAcronym(int);
+      case "hbs": return slashFunHBS(int);
+      case "color": return slashFunColor(int);
+      case "buttermelon": return slashFunButtermelon(int);
+      case "quote": return slashFunQuote(int);
+      case "namegame": return slashFunNamegame(int);
+      case "choose": return slashFunChoose(int);
+      case "emoji": return slashFunEmoji(int);
+      default: return u.errorHandler(new Error("Unhandled Subcommand"), int);
+    }
+  }
+})
+.addEvent("messageCreate", buttermelonEdit)
+.addEvent("messageUpdate", (oldMsg, msg) => {
+  if (oldMsg.partial || !(oldMsg.cleanContent.toLowerCase().includes("banana"))) {
+    buttermelonEdit(msg);
+  }
+})
+.setInit((data) => {
+  if (data) {
+    storedChoice = data.storedChoice;
+    storedChooser = data.storedChooser;
+  }
+})
+.setUnload(() => {
+  return { storedChoice, storedChooser };
+// })
+// .addEvent(
+//   "messageReactionAdd",
+//   (reaction) => { // could have (reaction, user) as args but lint don't like unused var.
+//     if ((reaction.message.channel.id == u.sf.channels.memes) && (reaction.emoji.name == "♻️")) { //memes channel id will have to be added if this is to be enabled, I don't know if it is still needed or even used by anyone.
+//       reaction.remove();
+//       reaction.message.react("⭐").catch(u.errorHandler);
+//     }
+});
+
+module.exports = Module;
