d3c5ca215face59576d46bedf47d7b6bf489a484
BobbyTheCatfish <46359040+BobbyTheCatfish@users.noreply.github.com>
2025-04-17 22:25:56 -0400

    Migrate tags (#109)
    * tags initial commit
    
    * tested tags and file saving, added /help
    
    /help only gets tags for now, as I can't see much of a reason for adding details for slash commands, and theres no easy way to filter permissions for message based commands
    
    * ignore media/tags contents
    
    * fix random var example
    
    * add team sf, fix /tag value
    
    * escape tag names in /help
    
    * update default permissions
    
    update default permissions to match new format for testing.
    
    * modal content
    
    changed content input method to modal for multiline tags
    suggested changes
    sorted autocomplete results
    
    * pagedembeds for help
    
    * consts to functions, cleaner
    
    Cleaned up a few things
    
    * better help, more /ldsg commands
    
    also fixed a big where the content was set to the previous created tag's content
    
    * migrated low boost alert while im at it
    
    * change youtube to socials
    
    * update to latest version
    
    * moved tags and common commands to util file
    
    this'll be useful for the site
    
    * new system
    
    * reply instead of send
    
    prevents people from deleting their commands and pretending like icarus is doing something on its own
    
    * use augur shared
    
    * use shared in help
    
    * ephemeral
    
    * string and embed changes
    
    * remove redundant command, other optimizations
    
    * whitespace and simplification
    
    * use updated paged response functions
    
    * few last comments
    
    * delete or upload attachments
    
    ---------
    
    Co-authored-by: LDSGOhGo <56556525+LDSGOhGo@users.noreply.github.com>

---
+++ database/controllers/tag.js
@@ -0,0 +1,37 @@
+// @ts-check
+const Tag = require("../models/Tag.model");
+
+/**
+ * @typedef tag
+ * @prop {string} tag the tag name
+ * @prop {string | null} [response] the tag response
+ * @prop {string | null} [attachment] the tag file name
+ * @prop {string | null} attachmentExtension the file extension for the file
+ * @prop {import("mongoose").Types.ObjectId} _id the id of the tag
+ */
+
+module.exports = {
+  /**
+   * Fetch all tags
+   * @returns {Promise<tag[]>}
+   */
+  fetchAllTags: function() {
+    return Tag.find({}, undefined, { lean: true }).exec();
+  },
+  /**
+   * Create or modify a tag
+   * @param {Omit<tag, "_id">} data tag data
+   * @returns {Promise<tag | null>}
+   */
+  manageTag: function(data) {
+    return Tag.findOneAndUpdate({ tag: data.tag }, data, { upsert: true, lean: true, new: true });
+  },
+  /**
+   * Delete a tag
+   * @param {string} tag the tag to delete
+   * @returns {Promise<tag | null>}
+   */
+  deleteTag: function(tag) {
+    return Tag.findOneAndRemove({ tag }, { new: false, lean: true }).exec();
+  }
+};
