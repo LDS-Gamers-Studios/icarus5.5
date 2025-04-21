dd1c3734019659ca19bc56060afcf5c626541644
BobbyTheCatfish <46359040+BobbyTheCatfish@users.noreply.github.com>
2025-04-21 11:54:53 -0400

    strings and optimizations

--- modules/missionmail.js
+++ modules/missionmail.js
@@ -5,8 +5,9 @@ const receive = require("imapflow");
 const interpret = require("mailparser-mit");
 const htmlparse = require("html-to-text");
 const Augur = require("augurbot-ts");
-const { ChannelType, ButtonStyle } = require("discord.js");
-const [approveIdPrefix, rejectIdPrefix] = ["approvemissionmail", "rejectmissionmail"];
+const { ButtonStyle } = require("discord.js");
+
+const [approveIdPrefix, rejectIdPrefix] = ["approveMissionMail", "rejectMissionMail"];
 
 const replyRegexes = [
   // /^on[\s\n\r]+.+wrote:[\s\n\r]*$/im,
@@ -25,13 +26,13 @@ const replyRegexes = [
 
 const Module = new Augur.Module();
 
-async function logNPull() {
+async function loadEmails() {
   const creds = c.google.mail;
   try {
     const receiver = new receive.ImapFlow({
       auth: {
         user: creds.email,
-        pass: creds.gAccountAppPass
+        pass: creds.pass
       },
       host: 'imap.gmail.com',
       port: 993,
@@ -42,10 +43,11 @@ async function logNPull() {
     await receiver.mailboxOpen("INBOX");
 
     const sent = await sendUnsent(receiver);
-    // receiver.close();
+    receiver.close();
+
     return sent;
   } catch (error) {
-    u.errorHandler(error, `login & pull missionmail`);
+    u.errorHandler(error, "Loading Emails");
   }
 }
 
@@ -59,13 +61,16 @@ async function sendUnsent(receiver) {
   const messageIds = await receiver.search({ or: u.db.sheets.missionaries.map((email) => ({ from: email })), seen: false });
   const messages = await receiver.fetchAll(messageIds, { source: true });
 
+  const approvals = Module.client.getTextChannel(u.sf.channels.missionary.approvals);
+  if (!approvals) throw new Error("unable to find approval channel for missionary emails.");
+
   for (const rawMsg of messages) {
 
     // parse the email source into readable stuff
     /** @type {interpret.ParsedEmail} */
-    const parsed = await new Promise((resolve) => {
+    const parsed = await new Promise((res) => {
       const parser = new interpret.MailParser();
-      parser.on("end", result => resolve(result));
+      parser.on("end", result => res(result));
       parser.write(rawMsg.source);
       parser.end();
     });
@@ -89,6 +94,7 @@ async function sendUnsent(receiver) {
         break; // Stop after the first match to avoid over-trimming
       }
     }
+
     parsed.text = parsed.text?.trim();
 
     // now that we've removed the reply...
@@ -96,18 +102,11 @@ async function sendUnsent(receiver) {
 
     // figure out who it is from
     const fromEmail = parsed.from ? parsed.from[0].address : "Err:NoFromAddress";
-    const missionaryId = u.db.sheets.missionaries.findKey(address => fromEmail?.includes(address) ? address : false);
+    const missionaryId = u.db.sheets.missionaries.findKey(address => fromEmail?.includes(address));
 
-    // get some discord side of things stuff
-    const ldsg = Module.client.guilds.cache.get(u.sf.ldsg);
-    const missionary = ldsg?.members.cache.get(missionaryId ?? "");
-    if (!missionary || !ldsg) {
-      // still need this in case the filter fails or they aren't a member anymore.
-      continue;
-    }
-
-    const missionMailApprovals = ldsg.channels.cache.get(u.sf.channels.missionary.approvals);
-    if (!missionMailApprovals || missionMailApprovals.type !== ChannelType.GuildText) throw new Error("unable to find approval channel for missionary emails.");
+    // get the server member. if they don't show up, they're probably not a member anymore
+    const missionary = Module.client.guilds.cache.get(u.sf.ldsg)?.members.cache.get(missionaryId ?? "");
+    if (!missionary) continue;
 
     const embed = u.embed({ author: missionary })
       .setTitle(`${missionary.displayName} - ${parsed.subject}`)
@@ -121,15 +120,15 @@ async function sendUnsent(receiver) {
     const actionRow = u.MessageActionRow().addComponents([approveBtn, rejectBtn]);
 
     for (const em of embeds) {
-      await missionMailApprovals.send({ embeds: [em] });
+      await approvals.send({ embeds: [em] });
     }
 
 
     const files = parsed.attachments?.slice(0, 9).map(a => new u.Attachment(a.content).setName(a.fileName ?? a.generatedFileName));
 
     // pop the question
-    await missionMailApprovals.send({
-      content: `incoming missionary email from ${missionary.toString()}(\`${fromEmail}\`)`,
+    await approvals.send({
+      content: `Missionary Email from ${missionary.toString()} (\`${fromEmail}\`)`,
       files,
       components: [actionRow]
     });
@@ -143,8 +142,9 @@ async function slashMissionaryPull(int) {
   try {
     await int.editReply("Pulling Emails.");
 
-    const pulled = await logNPull();
-    int.editReply(`Pulled ${pulled} Emails`);
+    const pullCount = await loadEmails();
+    int.editReply(`Pulled ${pullCount} Emails`);
+
   } catch (error) {
     u.errorHandler(error, "slashMissionaryPull");
     return await int.editReply("Error pulling emails.");
@@ -156,7 +156,8 @@ async function slashMissionaryRegister(int) {
   const user = int.options.getUser("user", true);
   const email = int.options.getString("email", true);
 
-  if (!email.endsWith("@missionary.org")) return int.editReply("missionary emails must be part of @missionary.org");
+  if (!email.endsWith("@missionary.org")) return int.editReply("Missionary emails must end with `@missionary.org`");
+
   if (u.db.sheets.missionaries.has(user.id)) {
     // update db entry
     const entry = u.db.sheets.data.missionaries.find(m => m.get("UserId") === user.id);
@@ -168,7 +169,7 @@ async function slashMissionaryRegister(int) {
   }
   u.db.sheets.missionaries.set(user.id, email);
 
-  return int.editReply(`I set ${user.toString()}'s email to \`${email}\`.`);
+  return int.editReply(`I set ${user}'s email to \`${email}\`.`);
 }
 
 /** @param {Augur.GuildInteraction<"CommandSlash">} int */
@@ -185,14 +186,14 @@ async function slashMissionaryRemove(int) {
 async function slashMissionaryCheck(int) {
   const user = int.options.getUser("user", true);
   const email = u.db.sheets.missionaries.get(user.id);
-  if (!email) return int.editReply(`${user} doesn't have an email set!`);
-  return int.editReply(`${user} has their email stored as \`${email}\`.`);
+  if (!email) return int.editReply(`${user} doesn't have a missionary email set!`);
+  return int.editReply(`${user} has their missionary email stored as \`${email}\`.`);
 }
 
 Module
-.setInit(logNPull)
+.setInit(loadEmails)
 .setClockwork(() => {
-  return setInterval(logNPull, 24 * 60 * 60_000);
+  return setInterval(loadEmails, 24 * 60 * 60_000);
 })
 .addInteraction({
   name: "missionary",
@@ -203,7 +204,6 @@ Module
   process: async (int) => {
     const subcommand = int.options.getSubcommand(true);
     await int.deferReply({ flags: u.ephemeralChannel(int, u.sf.channels.missionary.approvals) });
-    if (!u.perms.calc(int.member, ["mod"])) return int.editReply("That command is only for mods.");
 
     switch (subcommand) {
       case "remove": return slashMissionaryRemove(int);
@@ -218,25 +218,26 @@ Module
   if (!int.inCachedGuild() || !int.isButton() || int.guild.id !== u.sf.ldsg) return;
 
   if (int.customId.startsWith(approveIdPrefix)) {
-    if (!u.perms.calc(int.member, ["mod"])) return int.reply({ content: "You don't have permissions to approve missionary emails!", flags: ["Ephemeral"] });
+    if (!u.perms.calc(int.member, ["mod"])) return int.reply({ content: "You don't have permissions to interact with this!", flags: ["Ephemeral"] });
 
     const embedPages = parseInt(int.customId.substring(approveIdPrefix.length));
     const pagedMessages = await int.channel?.messages.fetch({ before: int.message.id, limit: embedPages });
     if (!pagedMessages) return int.reply({ content: "I couldn't find the messages to forward!" });
 
-    await int.update({ content: int.message.content + `\n(approved by ${int.user})`, components: [] });
+    await int.update({ content: int.message.content + `\n\nApproved by ${int.user}`, components: [] });
 
     for (const message of pagedMessages.reverse().values()) {
       await int.client.getTextChannel(u.sf.channels.missionary.mail)?.send({ embeds: message.embeds, files: [...message.attachments.values()] });
     }
+
     int.client.getTextChannel(u.sf.channels.missionary.mail)?.send({ embeds: int.message.embeds, files: [...int.message.attachments.values()] });
     return true;
   }
 
   if (int.customId.startsWith(rejectIdPrefix)) {
-    if (!u.perms.calc(int.member, ["mod"])) return int.reply({ content: "You don't have permissions to reject missionary emails!", flags: ["Ephemeral"] });
+    if (!u.perms.calc(int.member, ["mod"])) return int.reply({ content: "You don't have permissions to interact with this!", flags: ["Ephemeral"] });
 
-    int.update({ content: int.message.content + `\n(rejected by ${int.user})`, components: [] });
+    int.update({ content: int.message.content + `\n\n Rejected by ${int.user}`, components: [] });
     return true;
   }
 });
